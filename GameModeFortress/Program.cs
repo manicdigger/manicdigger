manicdigger/manicdigger

commit e08033641b1b1819125e1603b8fea66668d88053

Fix: Use server port when connecting to .mdlink.

GameModeFortress/Program.cs
@@ -46,6 +46,7 @@ namespace GameModeFortress
     public class ManicDiggerProgram2 : IInternetGameFactory
     {
         public string GameUrl = null;
+        public string User;
         ManicDiggerGameWindow w;
         AudioOpenAl audio;
         public void Start()
@@ -170,7 +171,6 @@ namespace GameModeFortress
             MakeGame(false);
         }
         #endregion
-        public string User;
     }
     public class ManicDiggerProgram
     {
@@ -199,6 +199,8 @@ namespace GameModeFortress
                         throw new Exception("Invalid game mode: " + mode);
                     }
                     p.GameUrl = XmlTool.XmlVal(d, "/ManicDiggerLink/Ip");
+                    int port = int.Parse(XmlTool.XmlVal(d, "/ManicDiggerLink/Port"));
+                    p.GameUrl += ":" + port;
                     p.User = XmlTool.XmlVal(d, "/ManicDiggerLink/User");
                 }
             }

GameWindow.cs
@@ -1703,11 +1703,21 @@ namespace ManicDigger
                 //b) just hash
                 //c) ip:port (server must have disabled authorization checking).
                 LoginData logindata = new LoginData();
+                int? pport = null;
+                if (qgameurl.Contains(":"))
+                {
+                    pport = int.Parse(qgameurl.Substring(qgameurl.IndexOf(":") + 1).Trim());
+                    qgameurl = qgameurl.Substring(0, qgameurl.IndexOf(":"));
+                }
                 System.Net.IPAddress server2 = null;
                 if (System.Net.IPAddress.TryParse(qgameurl, out server2))
                 {
                     logindata.serveraddress = server2.ToString();
                     logindata.port = 25565;
+                    if (pport != null)
+                    {
+                        logindata.port = pport.Value;
+                    }
                     logindata.mppass = "";
                 }
                 else

GameModeMine/Program.cs
@@ -149,6 +149,8 @@ namespace ManicDigger
                         throw new Exception("Invalid game mode: " + mode);
                     }
                     p.GameUrl = XmlTool.XmlVal(d, "/ManicDiggerLink/Ip");
+                    int port = int.Parse(XmlTool.XmlVal(d, "/ManicDiggerLink/Port"));
+                    p.GameUrl += ":" + port;
                     p.User = XmlTool.XmlVal(d, "/ManicDiggerLink/User");
                 }
             }

commit e5710a874c2b01afa1ce91e2bc36a1f41f856335

Mine mode: Fixed connecting to Minecraft servers using http address. (broken in revision e08033641b1b1819125e1603b8fea66668d88053 2010-05-30 15:16:51)

GameModeFortress/Program.cs
@@ -157,7 +157,7 @@ namespace GameModeFortress
             clientgame.worldgeneratorsandbox = gen;
             clientgame.minecartdrawer = new MinecartDrawer() { the3d = the3d, getfile = getfile };
             w.game = clientgame;
-            w.login = new LoginClientMinecraft();
+            w.login = new LoginClientDummy();
             w.internetgamefactory = internetgamefactory;
             physics.map = clientgame.mapforphysics;
             physics.data = gamedata;

GameWindow.cs
@@ -1535,12 +1535,24 @@ namespace ManicDigger
                 //c) ip:port (server must have disabled authorization checking).
                 LoginData logindata = new LoginData();
                 int? pport = null;
-                if (qgameurl.Contains(":"))
+                if (qgameurl.Contains(":") && (!qgameurl.Contains("http")))
                 {
                     pport = int.Parse(qgameurl.Substring(qgameurl.IndexOf(":") + 1).Trim());
                     qgameurl = qgameurl.Substring(0, qgameurl.IndexOf(":"));
                 }
                 System.Net.IPAddress server2 = null;
+                try
+                {
+                    logindata = login.Login(qusername, qpass, qgameurl);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+                if (logindata == null)
+                {
+                    logindata = new LoginData();
+                }
                 if (System.Net.IPAddress.TryParse(qgameurl, out server2))
                 {
                     logindata.serveraddress = server2.ToString();
@@ -1549,11 +1561,10 @@ namespace ManicDigger
                     {
                         logindata.port = pport.Value;
                     }
-                    logindata.mppass = "";
-                }
-                else
-                {
-                    logindata = login.Login(qusername, qpass, qgameurl);
+                    if (logindata.mppass == null)
+                    {
+                        logindata.mppass = "";
+                    }
                 }
                 frametickmainthreadtodo.Add(
                     () =>

LoginClient.cs
@@ -17,6 +17,15 @@ namespace ManicDigger
     {
         LoginData Login(string username, string password, string gameurl);
     }
+    public class LoginClientDummy : ILoginClient
+    {
+        #region ILoginClient Members
+        public LoginData Login(string username, string password, string gameurl)
+        {
+            return null;
+        }
+        #endregion
+    }
     public class LoginClientMinecraft : ILoginClient
     {
         //Three Steps