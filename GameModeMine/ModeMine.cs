07780ffd1951c401bac6edcbb9fbaa1ecdeafc0c
exe <none@example.com>
2010-07-08 16:19:45 +0200

    Shadows fixes in mine mode - still slow and broken. Allow to build iron block, gold block, flower decorations in mine mode.

--- GameModeMine/ModeMine.cs
+++ GameModeMine/ModeMine.cs
@@ -38,7 +38,10 @@ namespace ManicDigger
             terrain.UpdateTile(x, y, z);
             if (ENABLE_SHADOWS)
             {
-                UpdateShadows(x, y, z);
+                lock (lighttoupdate_lock)
+                {
+                    UpdateShadows(x, y, z);
+                }
             }
         }
         private void UpdateShadows(int x, int y, int z)
@@ -46,6 +49,7 @@ namespace ManicDigger
             lighttoupdate.Clear();
             UpdateSunlight(x, y, z);
             List<Vector3i> near = new List<Vector3i>();
+            near.Add(new Vector3i(x, y, z));
             foreach (var n in BlocksNear(x, y, z))
             {
                 if (MapUtil.IsValidPos(map, n.x, n.y, n.z))
@@ -63,6 +67,7 @@ namespace ManicDigger
             }
         }
         Dictionary<Vector3i, Vector3i> lighttoupdate = new Dictionary<Vector3i, Vector3i>();
+        object lighttoupdate_lock = new object();
         public void SendSetBlock(Vector3 vector3, BlockSetMode blockSetMode, int p)
         {
             network.SendSetBlock(vector3, blockSetMode, p);
@@ -159,6 +164,21 @@ namespace ManicDigger
         public void UseMap(byte[, ,] map)
         {
             this.map.Map = map;
+            MapSizeX = map.GetUpperBound(0) + 1;
+            MapSizeY = map.GetUpperBound(1) + 1;
+            MapSizeZ = map.GetUpperBound(2) + 1;
+            ResetShadows();
+            loaded = true;
+        }
+        private void ResetShadows()
+        {
+            if (ENABLE_SHADOWS)
+            {
+                light = null;
+                lightheight = null;
+                chunklighted = null;
+                UpdateHeightCache();
+            }
         }
         #endregion
         MapManipulator mapmanipulator = new MapManipulator();
@@ -170,10 +190,7 @@ namespace ManicDigger
         public void LoadState(byte[] savegame)
         {
             mapmanipulator.LoadMap(map, savegame);
-            if (ENABLE_SHADOWS)
-            {
-                UpdateHeightCache();
-            }
+            ResetShadows();
             loaded = true;
         }
         bool loaded = false;
@@ -195,16 +212,6 @@ namespace ManicDigger
                         {
                             light[x, y, z] = (byte)minlight;
                         }
-                    }
-                }
-            }
-            for (int x = 0; x < MapSizeX; x++)
-            {
-                for (int y = 0; y < MapSizeY; y++)
-                {
-                    for (int z = 0; z < MapSizeZ; z++)
-                    {
-                        FloodLight(x, y, z);
                     }
                 }
             }
@@ -243,24 +250,26 @@ namespace ManicDigger
         int minlight = 1;
         int maxlight = 10;
         int defaultshadow = 7;
-        void UpdateSunlight(int x, int y,int z)
+        void UpdateSunlight(int x, int y, int z)
         {
             int oldheight = lightheight[x, y];
+            if (oldheight < 0) { oldheight = 0; }
+            if (oldheight >= map.MapSizeZ) { oldheight = map.MapSizeZ - 1; }
             UpdateLightHeightmapAt(x, y);
             int newheight = lightheight[x, y];
-            if (newheight > oldheight)
+            if (newheight < oldheight)
             {
                 //make white
-                for (int i = oldheight; i <= newheight; i++)
+                for (int i = newheight; i <= oldheight; i++)
                 {
-                    SetLight(x, y, i, defaultshadow);
+                    SetLight(x, y, i, maxlight);
                     FloodLight(x, y, i);
                 }
             }
-            if (newheight < oldheight)
+            if (newheight > oldheight)
             {
                 //make black
-                for (int i = newheight; i <= oldheight; i++)
+                for (int i = oldheight; i <= newheight; i++)
                 {
                     SetLight(x, y, i, minlight);
                     //DefloodLight(x, i);
@@ -283,8 +292,16 @@ namespace ManicDigger
         void SetLight(int x,int y,int z, int value)
         {
             light[x, y, z] = (byte)value;
-            //todo
             lighttoupdate[new Vector3i((x / 16) * 16 + 5, (y / 16) * 16 + 5, (z / 16) * 16 + 5)] = new Vector3i();
+            foreach(Vector3i v in BlocksNear(x,y,z))
+            {
+                if (v.x / 16 != x / 16
+                    || v.y / 16 != y / 16
+                    || v.z / 16 != z / 16)
+                {
+                    lighttoupdate[new Vector3i((v.x / 16) * 16 + 5, (v.y / 16) * 16 + 5, (v.z / 16) * 16 + 5)] = new Vector3i();
+                }
+            }
         }
         private void DefloodLight(IEnumerable<Vector3i> start)
         {
@@ -307,7 +324,8 @@ namespace ManicDigger
                 {
                     continue;
                 }
-                if (!data.GrassGrowsUnder(map.GetBlock(v.x, v.y, v.z)))
+                if (!data.GrassGrowsUnder(map.GetBlock(v.x, v.y, v.z))
+                    && !IsLightEmitting(map.GetBlock(v.x, v.y, v.z)))
                 {
                     continue;
                 }
@@ -351,6 +369,10 @@ namespace ManicDigger
             {
                 UpdateLight();
             }
+            if (IsLightEmitting(GetBlock(x, y, z)))
+            {
+                light[x, y, z] = (byte)(maxlight);
+            }
             Queue<Vector3i> q = new Queue<Vector3i>();
             q.Enqueue(new Vector3i(x, y, z));
             for (; ; )
@@ -368,7 +390,8 @@ namespace ManicDigger
                 {
                     continue;
                 }
-                if (!data.GrassGrowsUnder(map.GetBlock(v.x, v.y, v.z)))
+                if (!data.GrassGrowsUnder(map.GetBlock(v.x, v.y, v.z))
+                    && !IsLightEmitting(map.GetBlock(v.x,v.y,v.z)))
                 {
                     continue;
                 }
@@ -386,8 +409,26 @@ namespace ManicDigger
                 }
             }
         }
+        private bool IsLightEmitting(int blocktype)
+        {
+            return blocktype == (int)TileTypeMinecraft.GoldBlock
+                || blocktype == (int)TileTypeMinecraft.Torch
+                || blocktype == (int)TileTypeMinecraft.Lava
+                || blocktype == (int)TileTypeMinecraft.StationaryLava
+                || blocktype == (int)TileTypeMinecraft.InfiniteLavaSource;
+        }
         private IEnumerable<Vector3i> BlocksNear(int x, int y, int z)
         {
+            yield return new Vector3i(x - 1, y, z);
+            yield return new Vector3i(x + 1, y, z);
+            yield return new Vector3i(x, y - 1, z);
+            yield return new Vector3i(x, y + 1, z);
+            yield return new Vector3i(x, y, z - 1);
+            yield return new Vector3i(x, y, z + 1);
+        }
+        private IEnumerable<Vector3i> BlocksNearWith(int x, int y, int z)
+        {
+            yield return new Vector3i(x, y, z);
             yield return new Vector3i(x - 1, y, z);
             yield return new Vector3i(x + 1, y, z);
             yield return new Vector3i(x, y - 1, z);
@@ -414,11 +455,55 @@ namespace ManicDigger
         #region IGameMode Members
         public double SIMULATIONLAG_SECONDS { get; set; }
         #endregion
+        const int chunksize = 16;
         #region ITerrainInfo Members
         public int GetTerrainBlock(int x, int y, int z)
         {
+            if (ENABLE_SHADOWS)
+            {
+                if (chunklighted == null)
+                {
+                    chunklighted = new bool[MapSizeX / chunksize, MapSizeY / chunksize, MapSizeZ / chunksize];
+                }
+                foreach (var k in BlocksNear(x / chunksize, y / chunksize, z / chunksize))
+                {
+                    if (!IsValidChunkPos(k.x, k.y, k.z))
+                    {
+                        continue;
+                    }
+                    if (!chunklighted[k.x, k.y, k.z])
+                    {
+                        lock (lighttoupdate_lock)
+                        {
+                            FloodLightChunk(k.x * chunksize, k.y * chunksize, k.z * chunksize);
+                            chunklighted[k.x, k.y, k.z] = true;
+                        }
+                    }
+                }
+            }
             return GetBlock(x, y, z);
         }
+        private void FloodLightChunk(int x, int y, int z)
+        {
+            for (int xx = 0; xx < chunksize; xx++)
+            {
+                for (int yy = 0; yy < chunksize; yy++)
+                {
+                    for (int zz = 0; zz < chunksize; zz++)
+                    {
+                        FloodLight(x + xx, y + yy, z + zz);
+                    }
+                }
+            }
+        }
+        bool IsValidChunkPos(int cx, int cy, int cz)
+        {
+            return cx >= 0 && cy >= 0 && cz >= 0
+                && cx < MapSizeX / chunksize
+                && cy < MapSizeY / chunksize
+                && cz < MapSizeZ / chunksize;
+        }
+        bool[, ,] chunklighted;
         public System.Drawing.Color GetTerrainBlockColor(int x, int y, int z)
         {
             return System.Drawing.Color.White;
@@ -603,13 +688,13 @@ namespace ManicDigger
             data[(int)TileTypeMinecraft.RedCloth + 13] = new TileTypeData() { Buildable = true, AllTextures = 64 + 13 };
             data[(int)TileTypeMinecraft.RedCloth + 14] = new TileTypeData() { Buildable = true, AllTextures = 64 + 14 };
             data[(int)TileTypeMinecraft.RedCloth + 15] = new TileTypeData() { Buildable = true, AllTextures = 64 + 15 };//36
-            data[(int)TileTypeMinecraft.YellowFlowerDecorations] = new TileTypeData() { Buildable = false, AllTextures = 13 };
-            data[(int)TileTypeMinecraft.RedRoseDecorations] = new TileTypeData() { Buildable = false, AllTextures = 12 };
-            data[(int)TileTypeMinecraft.RedMushroom] = new TileTypeData() { Buildable = false, AllTextures = 28 };
-            data[(int)TileTypeMinecraft.BrownMushroom] = new TileTypeData() { Buildable = false, AllTextures = 29 };
+            data[(int)TileTypeMinecraft.YellowFlowerDecorations] = new TileTypeData() { Buildable = true, AllTextures = 13 };
+            data[(int)TileTypeMinecraft.RedRoseDecorations] = new TileTypeData() { Buildable = true, AllTextures = 12 };
+            data[(int)TileTypeMinecraft.RedMushroom] = new TileTypeData() { Buildable = true, AllTextures = 28 };
+            data[(int)TileTypeMinecraft.BrownMushroom] = new TileTypeData() { Buildable = true, AllTextures = 29 };
             data[(int)TileTypeMinecraft.Lava] = new TileTypeData() { Buildable = false, AllTextures = 30 };
-            data[(int)TileTypeMinecraft.GoldBlock] = new TileTypeData() { Buildable = false, AllTextures = 24 };
-            data[(int)TileTypeMinecraft.IronBlock] = new TileTypeData() { Buildable = false, AllTextures = 23 };
+            data[(int)TileTypeMinecraft.GoldBlock] = new TileTypeData() { Buildable = true, AllTextures = 24 };
+            data[(int)TileTypeMinecraft.IronBlock] = new TileTypeData() { Buildable = true, AllTextures = 23 };
             data[(int)TileTypeMinecraft.DoubleStair] = new TileTypeData() { Buildable = true, AllTextures = (0 * 16) + 5, TextureTop = 6 };//43 todo
             data[(int)TileTypeMinecraft.Stair] = new TileTypeData() { Buildable = true, AllTextures = 6 };//44
             data[(int)TileTypeMinecraft.TNT] = new TileTypeData()
@@ -633,7 +718,7 @@ namespace ManicDigger
             //fire todo
             data[(int)TileTypeMinecraft.InfiniteWaterSource] = new TileTypeData() { Buildable = false, AllTextures = 14 };//52
             data[(int)TileTypeMinecraft.InfiniteLavaSource] = new TileTypeData() { Buildable = false, AllTextures = 30 };//53
-            data[(int)TileTypeMinecraft.Chest] = new TileTypeData() { Buildable = true, AllTextures = 4 };//54
+            data[(int)TileTypeMinecraft.Chest] = new TileTypeData() { Buildable = false, AllTextures = 4 };//54
             //gear todo
             //diamond todo
             //diamond block todo
