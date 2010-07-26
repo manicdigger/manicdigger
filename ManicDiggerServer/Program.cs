f38b3fd655a43d7c461eca2d22144bb7bd3b722d
exe <none@example.com>
2010-07-26 13:13:14 +0200

    Fixed server crash on start.

--- ManicDiggerServer/Program.cs
+++ ManicDiggerServer/Program.cs
@@ -870,7 +870,9 @@ namespace ManicDiggerServer
             gen.Compile(g.generator, seed);
             g.Seed = seed;
             s.players = g;
-            g.shadows = new ShadowsSimple() { data = data, map = g };
+            var shadows = new ShadowsSimple() { data = data, map = g };
+            g.shadows = shadows;
+            g.map.shadows = shadows;
 
             s.Start();
             new Thread((a) => { for (; ; ) { s.SendHeartbeat(); Thread.Sleep(TimeSpan.FromMinutes(1)); } }).Start();
