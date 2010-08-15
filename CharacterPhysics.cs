da127d3e8c55fb31b3d1141cc2bad86207ef1edb
exe <none@example.com>
2010-08-15 03:08:34 +0200

    Refactoring: Moved CharacterPhysics class to CharacterPhysics.cs. Renamed Water class to WaterSimple and moved to WaterSimple.cs.

---
+++ CharacterPhysics.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK;
+
+namespace ManicDigger
+{
+    public class CharacterPhysicsState
+    {
+        public float movedz = 0;
+        public Vector3 playerposition = new Vector3(15.5f, 40, 15.5f);
+        public Vector3 playerorientation = new Vector3((float)Math.PI, 0, 0);
+    }
+    public class CharacterPhysics
+    {
+        [Inject]
+        public IMapStorage map { get; set; }
+        [Inject]
+        public IGameData data { get; set; }
+        void Update()
+        {
+        }
+        bool IsTileEmptyForPhysics(int x, int y, int z)
+        {
+            if (z >= map.MapSizeZ)
+            {
+                return true;
+            }
+            bool ENABLE_FREEMOVE = false;
+            if (x < 0 || y < 0 || z < 0)// || z >= mapsizez)
+            {
+                return ENABLE_FREEMOVE;
+            }
+            if (x >= map.MapSizeX || y >= map.MapSizeY)// || z >= mapsizez)
+            {
+                return ENABLE_FREEMOVE;
+            }
+            //this test is so the player does not walk on water.
+            if (data.IsWaterTile(map.GetBlock(x, y, z)) &&
+                !data.IsWaterTile(map.GetBlock(x, y, z + 1))) { return true; }
+            return map.GetBlock(x, y, z) == data.TileIdEmpty
+                || map.GetBlock(x, y, z) == data.TileIdSingleStairs
+                || (data.IsWaterTile(map.GetBlock(x, y, z)) && (!swimmingtop))
+                || data.IsEmptyForPhysics(map.GetBlock(x, y, z));
+        }
+        public static float walldistance = 0.3f;
+        public static float characterheight = 1.5f;
+        public void Move(CharacterPhysicsState state, double dt)
+        {
+        }
+        public Vector3 WallSlide(Vector3 oldposition, Vector3 newposition)
+        {
+            reachedceiling = false;
+            reachedwall = false;
+            //Math.Floor() is needed because casting negative values to integer is not floor.
+            Vector3i oldpositioni = new Vector3i((int)Math.Floor(oldposition.X), (int)Math.Floor(oldposition.Z),
+                (int)Math.Floor(oldposition.Y));
+            bool wasonstairs = false;
+            if (MapUtil.IsValidPos(map, oldpositioni.x, oldpositioni.y, oldpositioni.z))
+            {
+                wasonstairs = map.GetBlock(oldpositioni.x, oldpositioni.y, oldpositioni.z) == data.TileIdSingleStairs;
+            }
+            Vector3 playerposition = newposition;
+            //left
+            {
+                var qnewposition = newposition + new Vector3(0, 0, walldistance);
+                bool newempty = IsTileEmptyForPhysics((int)Math.Floor(qnewposition.X), (int)Math.Floor(qnewposition.Z), (int)Math.Floor(qnewposition.Y))
+                && IsTileEmptyForPhysics((int)Math.Floor(qnewposition.X), (int)Math.Floor(qnewposition.Z), (int)Math.Floor(qnewposition.Y) + 1);
+                if (newposition.Z - oldposition.Z > 0)
+                {
+                    if (!wasonstairs)
+                    {
+                        if (!newempty)
+                        {
+                            reachedwall = true;
+                            playerposition.Z = oldposition.Z;
+                        }
+                    }
+                    else
+                    {
+                        if (!newempty)
+                        {
+                            playerposition.Y += 0.5f;
+                            goto ok;
+                        }
+                    }
+                }
+            }
+            //front
+            {
+                var qnewposition = newposition + new Vector3(walldistance, 0, 0);
+                bool newempty = IsTileEmptyForPhysics((int)Math.Floor(qnewposition.X), (int)Math.Floor(qnewposition.Z), (int)Math.Floor(qnewposition.Y))
+                && IsTileEmptyForPhysics((int)Math.Floor(qnewposition.X), (int)Math.Floor(qnewposition.Z), (int)Math.Floor(qnewposition.Y) + 1);
+                if (newposition.X - oldposition.X > 0)
+                {
+                    if (!wasonstairs)
+                    {
+                        if (!newempty)
+                        {
+                            reachedwall = true;
+                            playerposition.X = oldposition.X;
+                        }
+
+                    }
+                    else
+                    {
+                        if (!newempty)
+                        {
+                            playerposition.Y += 0.5f;
+                            goto ok;
+                        }
+                    }
+                }
+            }
+            //top
+            {
+                var qnewposition = newposition + new Vector3(0, -walldistance, 0);
+                int x = (int)Math.Floor(qnewposition.X);
+                int y = (int)Math.Floor(qnewposition.Z);
+                int z = (int)Math.Floor(qnewposition.Y);
+                float a = walldistance;
+                bool newfull = (!IsTileEmptyForPhysics(x, y, z))
+                    || (qnewposition.X - Math.Floor(qnewposition.X) <= a && (!IsTileEmptyForPhysics(x - 1, y, z)) && (IsTileEmptyForPhysics(x - 1, y, z + 1)))
+                    || (qnewposition.X - Math.Floor(qnewposition.X) >= (1 - a) && (!IsTileEmptyForPhysics(x + 1, y, z)) && (IsTileEmptyForPhysics(x + 1, y, z + 1)))
+                    || (qnewposition.Z - Math.Floor(qnewposition.Z) <= a && (!IsTileEmptyForPhysics(x, y - 1, z)) && (IsTileEmptyForPhysics(x, y - 1, z + 1)))
+                    || (qnewposition.Z - Math.Floor(qnewposition.Z) >= (1 - a) && (!IsTileEmptyForPhysics(x, y + 1, z)) && (IsTileEmptyForPhysics(x, y + 1, z + 1)));
+                if (newposition.Y - oldposition.Y < 0)
+                {
+                    if (newfull)
+                    {
+                        playerposition.Y = oldposition.Y;
+                    }
+                }
+            }
+            //right
+            {
+                var qnewposition = newposition + new Vector3(0, 0, -walldistance);
+                bool newempty = IsTileEmptyForPhysics((int)Math.Floor(qnewposition.X), (int)Math.Floor(qnewposition.Z), (int)Math.Floor(qnewposition.Y))
+                && IsTileEmptyForPhysics((int)Math.Floor(qnewposition.X), (int)Math.Floor(qnewposition.Z), (int)Math.Floor(qnewposition.Y) + 1);
+                if (newposition.Z - oldposition.Z < 0)
+                {
+                    if (!wasonstairs)
+                    {
+                        if (!newempty)
+                        {
+                            reachedwall = true;
+                            playerposition.Z = oldposition.Z;
+                        }
+                    }
+                    else
+                    {
+                        if (!newempty)
+                        {
+                            playerposition.Y += 0.5f;
+                            goto ok;
+                        }
+                    }
+                }
+            }
+            //back
+            {
+                var qnewposition = newposition + new Vector3(-walldistance, 0, 0);
+                bool newempty = IsTileEmptyForPhysics((int)Math.Floor(qnewposition.X), (int)Math.Floor(qnewposition.Z), (int)Math.Floor(qnewposition.Y))
+                && IsTileEmptyForPhysics((int)Math.Floor(qnewposition.X), (int)Math.Floor(qnewposition.Z), (int)Math.Floor(qnewposition.Y) + 1);
+                if (newposition.X - oldposition.X < 0)
+                {
+                    if (!wasonstairs)
+                    {
+                        if (!newempty)
+                        {
+                            reachedwall = true;
+                            playerposition.X = oldposition.X;
+                        }
+                    }
+                    else
+                    {
+                        if (!newempty)
+                        {
+                            playerposition.Y += 0.5f;
+                            goto ok;
+                        }
+                    }
+                }
+            }
+            //bottom
+            {
+                var qnewposition = newposition + new Vector3(0, +walldistance + characterheight, 0);
+                bool newempty = IsTileEmptyForPhysics((int)Math.Floor(qnewposition.X), (int)Math.Floor(qnewposition.Z), (int)Math.Floor(qnewposition.Y));
+                if (newposition.Y - oldposition.Y > 0)
+                {
+                    if (!newempty)
+                    {
+                        playerposition.Y = oldposition.Y;
+                        reachedceiling = true;
+                    }
+                }
+            }
+        ok:
+            bool isonstairs = false;
+            Vector3i playerpositioni = new Vector3i((int)Math.Floor(playerposition.X), (int)Math.Floor(playerposition.Z),
+                 (int)Math.Floor(playerposition.Y));
+            if (MapUtil.IsValidPos(map, playerpositioni.x, playerpositioni.y, playerpositioni.z))
+            {
+                isonstairs = map.GetBlock(playerpositioni.x, playerpositioni.y, playerpositioni.z) == data.TileIdSingleStairs;
+            }
+            if (isonstairs)
+            {
+                playerposition.Y = ((int)Math.Floor(playerposition.Y)) + 0.5f + walldistance;
+            }
+            return playerposition;
+        }
+        public bool swimmingtop;
+        public bool reachedceiling;
+        public bool reachedwall;
+    }
+}
---
+++ WaterSimple.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK;
+
+namespace ManicDigger
+{
+    //todo water and sponges may not work when map is saved during flooding and then loaded.
+    public class WaterSimple
+    {
+        [Inject]
+        public IGameData data { get; set; }
+        public void Update()
+        {
+            if ((DateTime.Now - lastflood).TotalSeconds > 1)
+            {
+                lastflood = DateTime.Now;
+                var curtoflood = new List<Vector3>(toflood.Keys);
+                foreach (var v in curtoflood)
+                {
+                    Flood(v);
+                    toflood.Remove(v);
+                }
+            }
+        }
+        int spongerange = 2;
+        bool IsSpongeNear(int x, int y, int z)
+        {
+            for (int xx = x - spongerange; xx <= x + spongerange; xx++)
+            {
+                for (int yy = y - spongerange; yy <= y + spongerange; yy++)
+                {
+                    for (int zz = z - spongerange; zz <= z + spongerange; zz++)
+                    {
+                        if (MapUtil.IsValidPos(map, xx, yy, zz) && map.GetBlock(xx, yy, zz) == data.TileIdSponge)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+        public void BlockChange(IMapStorage map, int x, int y, int z)
+        {
+            this.flooded = new Dictionary<Vector3, Vector3>();
+            this.map = map;
+            //sponge just built.
+            if (MapUtil.IsValidPos(map, x, y, z) && map.GetBlock(x, y, z) == data.TileIdSponge)
+            {
+                for (int xx = x - spongerange; xx <= x + spongerange; xx++)
+                {
+                    for (int yy = y - spongerange; yy <= y + spongerange; yy++)
+                    {
+                        for (int zz = z - spongerange; zz <= z + spongerange; zz++)
+                        {
+                            if (MapUtil.IsValidPos(map, xx, yy, zz) && data.IsWaterTile(map.GetBlock(xx, yy, zz)))
+                            {
+                                tosetempty.Add(new Vector3(xx, yy, zz));
+                            }
+                        }
+                    }
+                }
+            }
+            //maybe sponge destroyed. todo faster test.
+            for (int xx = x - spongerange; xx <= x + spongerange; xx++)
+            {
+                for (int yy = y - spongerange; yy <= y + spongerange; yy++)
+                {
+                    for (int zz = z - spongerange; zz <= z + spongerange; zz++)
+                    {
+                        if (MapUtil.IsValidPos(map, xx, yy, zz) && map.GetBlock(xx, yy, zz) == data.TileIdEmpty)
+                        {
+                            BlockChangeFlood(map, xx, yy, zz);
+                        }
+                    }
+                }
+            }
+            BlockChangeFlood(map, x, y, z);
+            var v = new Vector3(x, y, z);
+            tosetwater.Sort((a, b) => (v - a).Length.CompareTo((v - b).Length));
+        }
+        void BlockChangeFlood(IMapStorage map, int x, int y, int z)
+        {
+            //water here
+            if (MapUtil.IsValidPos(map, x, y, z)
+                && data.IsWaterTile(map.GetBlock(x, y, z)))
+            {
+                Flood(new Vector3(x, y, z));
+                return;
+            }
+            //water around
+            foreach (var vv in BlocksAround(new Vector3(x, y, z)))
+            {
+                if (MapUtil.IsValidPos(map, (int)vv.X, (int)vv.Y, (int)vv.Z) &&
+                    data.IsWaterTile(map.GetBlock((int)vv.X, (int)vv.Y, (int)vv.Z)))
+                {
+                    Flood(vv);
+                    return;
+                }
+            }
+        }
+        IMapStorage map;
+        Dictionary<Vector3, Vector3> flooded = new Dictionary<Vector3, Vector3>();
+        public List<Vector3> tosetwater = new List<Vector3>();
+        public List<Vector3> tosetempty = new List<Vector3>();
+        Dictionary<Vector3, Vector3> toflood = new Dictionary<Vector3, Vector3>();
+        DateTime lastflood;
+        private void Flood(Vector3 v)
+        {
+            if (!MapUtil.IsValidPos(map, (int)v.X, (int)v.Y, (int)v.Z))
+            {
+                return;
+            }
+            if (flooded.ContainsKey(v))
+            {
+                return;
+            }
+            flooded.Add(v, v);
+            foreach (Vector3 vv in BlocksAround(v))
+            {
+                if (!MapUtil.IsValidPos(map, (int)vv.X, (int)vv.Y, (int)vv.Z))
+                {
+                    continue;
+                }
+                var type = map.GetBlock((int)vv.X, (int)vv.Y, (int)vv.Z);
+                if (type == data.TileIdEmpty && (!IsSpongeNear((int)vv.X, (int)vv.Y, (int)vv.Z)))
+                {
+                    tosetwater.Add(vv);
+                    toflood[vv] = vv;
+                }
+            }
+        }
+        IEnumerable<Vector3> BlocksAround(Vector3 pos)
+        {
+            yield return pos + new Vector3(-1, 0, 0);
+            yield return pos + new Vector3(1, 0, 0);
+            yield return pos + new Vector3(0, -1, 0);
+            yield return pos + new Vector3(0, 1, 0);
+            yield return pos + new Vector3(0, 0, -1);
+            //yield return pos + new Vector3(0, 0, 1); //water does not flow up.
+        }
+    }
+}
