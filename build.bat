5b15f6719d5b43951ab589e78653fc000fcfe0e0
exe <none@example.com>
2010-09-12 19:51:32 +0200

    Refactoring: Moved main project files into ManicDigger subfolder.

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ copy oalinst.exe output\
 xcopy /s data output\data\
 
 REM Dll
-xcopy /s bin\release\*.dll output\
+xcopy /s ManicDigger\bin\release\*.dll output\
 
 REM Mine mode
 xcopy /s /y GameModeMine\bin\release\*.dll output\
@@ -23,7 +23,7 @@ xcopy /s /y Start\bin\release\*.exe output\
 
 xcopy /y /s lib\*.dll output\
 del output\*vshost.exe
-copy menu.mdxs.gz output\menu.mdxs.gz
-copy WorldGenerator.cs output\WorldGenerator.cs
+REM copy menu.mdxs.gz output\menu.mdxs.gz
+REM copy WorldGenerator.cs output\WorldGenerator.cs
 copy credits.html output\credits.html
 REM pause
