8d595e23e93a940790e58b1d5936ac37b5ccc0e5
James Likier <jlikier@gmail.com>
2010-10-05 16:45:33 -0600

    Got rid of the ManicDiggerServer, it was unused.  Renamed Start project to GameLauncher, for more clearer naming.

--- GameLauncher/Properties/AssemblyInfo.cs
+++ GameLauncher/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Start")]
+[assembly: AssemblyTitle("GameLauncher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Start")]
+[assembly: AssemblyProduct("GameLauncher")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
