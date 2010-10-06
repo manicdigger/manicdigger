62ea80298ae655a78fc94dd39342a0c4cbb3bd34
James Likier <jlikier@gmail.com>
2010-10-06 07:30:19 -0600

    Starting the baseline for loading any type of game mode from one central application.

---
+++ GameClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameModeFortress;
+using ManicDigger;
+
+namespace GameClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IInternetGameFactory game = new ManicDiggerProgram2(); 
+        }
+    }
+}
