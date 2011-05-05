8459f8dbaa3812a56d6de39b4b58f527ee9854f3
exe <none@example.com>
2011-05-05 10:08:38 +0200

    Added tests: font, audio, inventory.

---
+++ Tests/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Input;
+using ManicDigger;
+using OpenTK.Graphics.OpenGL;
+
+namespace ManicDigger.Tests
+{
+    public class WindowTests : GameWindow, IViewportSize
+    {
+        public WindowTests(GraphicsMode mode)
+            : base(800, 600, mode)
+        {
+            VSync = VSyncMode.On;
+            WindowState = WindowState.Normal;
+            MainMenu();
+        }
+
+        private void MainMenu()
+        {
+            currentTest = new ListOfTests() { window = this, viewportsize = this };
+            currentTest.OnLoad(null);
+        }
+
+        public IMyGameWindow currentTest;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            currentTest.OnResize(e);
+        }
+        double esctime = 0;
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            if (Keyboard[Key.Escape])
+            {
+                MainMenu();
+                esctime += e.Time;
+                if (esctime > 0.5) { Exit(); }
+            }
+            else
+            {
+                esctime = 0;
+            }
+            currentTest.OnUpdateFrame(e);
+        }
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            currentTest.OnKeyPress(e);
+        }
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.ClearColor(Color4.Black);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            currentTest.OnRenderFrame(e);
+            SwapBuffers();
+        }
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            GraphicsMode mode = new GraphicsMode(new OpenTK.Graphics.ColorFormat(32), 24, 0, 2, new OpenTK.Graphics.ColorFormat(32));
+            using (WindowTests game = new WindowTests(mode))
+            {
+                game.Run(60.0);
+            }
+        }
+    }
+    public abstract class MyGameWindow : IMyGameWindow
+    {
+        public virtual void OnClosed(EventArgs e) { }
+        public virtual void OnFocusedChanged(EventArgs e) { }
+        public virtual void OnKeyPress(KeyPressEventArgs e) { }
+        public virtual void OnLoad(EventArgs e) { }
+        public virtual void OnRenderFrame(FrameEventArgs e) { }
+        public virtual void OnResize(EventArgs e) { }
+        public virtual void OnUpdateFrame(FrameEventArgs e) { }
+    }
+}
---
+++ Tests/TestFont.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ManicDigger;
+using OpenTK;
+using ManicDigger.Renderers;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+
+namespace ManicDigger.Tests
+{
+    public class TestFont : MyGameWindow
+    {
+        public GameWindow window;
+        public IGetFileStream getfile;
+        public IViewportSize viewportsize;
+        public override void OnLoad(EventArgs e)
+        {
+            the3d.d_Config3d = new Config3d();
+            the3d.d_GetFile = getfile;
+            the3d.d_Terrain = new TerrainTextures();
+            the3d.d_TextRenderer = new TextRenderer();
+            the3d.d_ViewportSize = viewportsize;
+        }
+        The3d the3d = new The3d();
+        Color[] colors = new Color[]
+		{
+			Color.Red, Color.Orange, Color.Yellow, Color.Green, Color.Blue, Color.Indigo, Color.Violet,
+		};
+        string[] colorCodes = new string[]
+		{
+			"&0","&1","&2","&3","&4","&5","&6","&7",
+			"&7","&9","&a","&b","&c","&d","&e","&f",
+		};
+        public override void OnRenderFrame(OpenTK.FrameEventArgs e)
+        {
+            GL.ClearColor(Color.Green);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            the3d.OrthoMode(window.Width, window.Height);
+            for (int i = 0; i < 7; i++)
+            {
+                the3d.Draw2dText("Hello!", 50, 100 + i * 50, 8 + i * 6, colors[i]);
+            }
+            for (int i = 0; i < 16; i++)
+            {
+                the3d.Draw2dText(colorCodes[i] + "Color" + "&f & " + colorCodes[i][1], 300, 100 + i * 30, 14, null);
+            }
+        }
+        public override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, window.Width, window.Height);
+            the3d.Set3dProjection();
+        }
+    }
+}
