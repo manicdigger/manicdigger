019667251b13ac195f1aaaeff4a0c9a0bd340ace
exe <none@example.com>
2011-05-06 09:48:06 +0200

    Performance: Cache data file path remap.

--- ManicDigger/Program.cs
+++ ManicDigger/Program.cs
@@ -31,50 +31,57 @@ namespace ManicDigger
         }
         public string[] DataPaths;
         Dictionary<string, byte[]> cache = new Dictionary<string, byte[]>();
+        Dictionary<string, string> remap = new Dictionary<string, string>();
         public Stream GetFile(string filename)
         {
+        retry:
+            if (remap.ContainsKey(filename))
+            {
+                filename = remap[filename];
+            }
             if (!cache.ContainsKey(filename))
             {
-				foreach (string path in DataPaths)
-				{
-					try
-					{
+                foreach (string path in DataPaths)
+                {
+                    try
+                    {
                         if (!Directory.Exists(path))
                         {
                             continue;
                         }
-						foreach (string s in Directory.GetFiles(path, "*.*", SearchOption.AllDirectories))
-						{
-							try
-							{
-								FileInfo f = new FileInfo(s);
-								cache[f.Name] = File.ReadAllBytes(s);
-							}
-							catch
-							{
-							}
-						}
-					}
-					catch
-					{
-					}
-				}
+                        foreach (string s in Directory.GetFiles(path, "*.*", SearchOption.AllDirectories))
+                        {
+                            try
+                            {
+                                FileInfo f = new FileInfo(s);
+                                cache[f.Name] = File.ReadAllBytes(s);
+                            }
+                            catch
+                            {
+                            }
+                        }
+                    }
+                    catch
+                    {
+                    }
+                }
             }
+            string origfilename = filename;
             for (int i = 0; i < 2; i++)
             {
                 if (cache.ContainsKey(filename)) { return new MemoryStream(cache[filename]); }
 
                 string f1 = filename.Replace(".png", ".jpg");
-                if (cache.ContainsKey(f1)) { return new MemoryStream(cache[f1]); }
+                if (cache.ContainsKey(f1)) { remap[origfilename] = f1; goto retry; }
 
                 string f2 = filename.Replace(".jpg", ".png");
-                if (cache.ContainsKey(f2)) { return new MemoryStream(cache[f2]); }
+                if (cache.ContainsKey(f2)) { remap[origfilename] = f2; goto retry; }
 
                 string f3 = filename.Replace(".wav", ".ogg");
-                if (cache.ContainsKey(f3)) { return new MemoryStream(cache[f3]); }
+                if (cache.ContainsKey(f3)) { remap[origfilename] = f3; goto retry; }
 
                 string f4 = filename.Replace(".ogg", ".wav");
-                if (cache.ContainsKey(f4)) { return new MemoryStream(cache[f4]); }
+                if (cache.ContainsKey(f4)) { remap[origfilename] = f4; goto retry; }
 
                 filename = new FileInfo(filename).Name; //handles GetFile(GetFile(file)) use.
             }
