083a980d5ab49eed1a4f171f66f6e17e6a64bbf7
rnd <nomail@example.com>
2012-07-14 15:26:41 +0200

    Config file for server monitor.

--- ManicDigger/ServerMonitor.cs
+++ ManicDigger/ServerMonitor.cs
@@ -1,6 +1,9 @@
 using System;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
 using ManicDigger;
 using ManicDiggerServer;
 using System.Threading;
@@ -9,18 +12,14 @@ namespace GameModeFortress
 {
     public class ServerMonitor
     {
-        public int MaxPackets = 500; // max number of packets - packet flood protection
-        public int MaxBlocks = 30; // max number of blocks which can be set within the time intervall
-        public int MaxMessages = 3; // max number of chat messages per time intervall
-        public int TimeIntervall = 3; // in seconds, resets count values
+        private ServerMonitorConfig config;
         public IGameExit Exit;
-        public TimeSpan MessageBanTime = new TimeSpan(0, 1, 0);// 1 minute
-
         private Server server;
         private Dictionary<int, MonitorClient> monitorClients;
 
         public ServerMonitor(Server server, IGameExit exit)
         {
+            this.LoadConfig();
             this.server = server;
             this.Exit = exit;
             this.monitorClients = new Dictionary<int, MonitorClient>();
@@ -36,7 +35,7 @@ namespace GameModeFortress
         {
             while(!Exit.exit)
             {
-                Thread.Sleep(TimeSpan.FromSeconds(TimeIntervall));
+                Thread.Sleep(TimeSpan.FromSeconds(config.TimeIntervall));
                 foreach (var k in monitorClients)
                 {
                     k.Value.BlocksSet = 0;
@@ -54,7 +53,7 @@ namespace GameModeFortress
             }
 
             monitorClients[clientId].PacketsReceived++;
-            if (monitorClients[clientId].PacketsReceived > MaxPackets)
+            if (monitorClients[clientId].PacketsReceived > config.MaxPackets)
             {
                 server.Kick(server.ServerConsoleId, clientId, "Packet Overflow");
                 return false;
@@ -69,7 +68,7 @@ namespace GameModeFortress
                         // TODO: revert block at client
                         return false;
                     }
-                    if (monitorClients[clientId].BlocksSet < MaxBlocks)
+                    if (monitorClients[clientId].BlocksSet < config.MaxBlocks)
                     {
                         monitorClients[clientId].BlocksSet++;
                         return true;
@@ -82,7 +81,7 @@ namespace GameModeFortress
                         server.SendMessage(clientId, "Spam protection: Your message has not been sent.", Server.MessageType.Error);
                         return false;
                     }
-                    if (monitorClients[clientId].MessagesSent < MaxMessages)
+                    if (monitorClients[clientId].MessagesSent < config.MaxMessages)
                     {
                         monitorClients[clientId].MessagesSent++;
                         return true;
@@ -105,8 +104,8 @@ namespace GameModeFortress
         }
         private bool ActionMessage(int clientId)
         {
-            this.monitorClients[clientId].MessagePunishment = new Punishment(MessageBanTime);
-            this.server.ServerMessageToAll(string.Format("Spam protection: {0} has been muted for {1} minutes.", server.GetClient(clientId).playername, MessageBanTime.TotalMinutes), Server.MessageType.Important);
+            this.monitorClients[clientId].MessagePunishment = new Punishment(new TimeSpan(0, 0, config.MessageBanTime));
+            this.server.ServerMessageToAll(string.Format("Spam protection: {0} has been muted for {1} seconds.", server.GetClient(clientId).playername, config.MessageBanTime), Server.MessageType.Important);
             return false;
         }
 
@@ -168,6 +167,82 @@ namespace GameModeFortress
                 }
                 return false;
             }
+        }
+
+
+        public class ServerMonitorConfig
+        {
+            public int MaxPackets; // max number of packets - packet flood protection
+            public int MaxBlocks; // max number of blocks which can be set within the time intervall
+            public int MaxMessages; // max number of chat messages per time intervall
+            public int MessageBanTime;// how long gets a player muted (in seconds)
+            public int TimeIntervall; // in seconds, resets count values
+
+            public ServerMonitorConfig()
+            {
+                //Set Defaults
+                this.MaxPackets = 500;
+                this.MaxBlocks = 30;
+                this.MaxMessages = 3;
+                this.MessageBanTime = 60;
+                this.TimeIntervall = 3;
+            }
+        }
+        string gamepathconfig = GameStorePath.GetStorePath();
+        string filename = "ServerMonitor.xml";
+        private void LoadConfig()
+        {
+            if (!File.Exists(Path.Combine(gamepathconfig, filename)))
+            {
+                Console.WriteLine("Server monitor configuration file not found, creating new.");
+                SaveConfig();
+            }
+            else
+            {
+                try
+                {
+                    using (TextReader textReader = new StreamReader(Path.Combine(gamepathconfig, filename)))
+                    {
+                        XmlSerializer deserializer = new XmlSerializer(typeof(ServerMonitorConfig));
+                        this.config = (ServerMonitorConfig)deserializer.Deserialize(textReader);
+                        textReader.Close();
+                        SaveConfig();
+                    }
+                }
+                catch //This if for the original format
+                {
+                    using (Stream s = new MemoryStream(File.ReadAllBytes(Path.Combine(gamepathconfig, filename))))
+                    {
+                        this.config = new ServerMonitorConfig();
+                        StreamReader sr = new StreamReader(s);
+                        XmlDocument d = new XmlDocument();
+                        d.Load(sr);
+                    }
+                    //Save with new version.
+                    SaveConfig();
+                }
+            }
+            Console.WriteLine("Server monitor configuration loaded.");
+        }
+        public void SaveConfig()
+        {
+            //Verify that we have a directory to place the file into.
+            if (!Directory.Exists(gamepathconfig))
+            {
+                Directory.CreateDirectory(gamepathconfig);
+            }
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ServerMonitorConfig));
+            TextWriter textWriter = new StreamWriter(Path.Combine(gamepathconfig, filename));
+
+            //Check to see if config has been initialized.
+            if (this.config == null)
+            {
+                this.config = new ServerMonitorConfig();
+            }
+            //Serialize the config class to XML.
+            serializer.Serialize(textWriter, this.config);
+            textWriter.Close();
         }
     }
 }
