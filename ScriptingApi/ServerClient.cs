c9bb33815cb83f5630bfae482ae0da578d93efd0
exe <exe@example.com>
2014-01-23 01:36:35 +0100

    Fixed compilation errors.

--- ScriptingApi/ServerClient.cs
+++ ScriptingApi/ServerClient.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Xml.Serialization;
 
-namespace GameModeFortress
+namespace ManicDigger
 {
     [XmlRoot(ElementName = "ManicDiggerServerClient")]
     public class ServerClient
