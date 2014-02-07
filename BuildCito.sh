87a2a6b1598297e3f8956e834f4162c4abf367e3
exe <exe@example.com>
2014-02-07 07:27:03 +0100

    Android.

--- BuildCito.sh
+++ BuildCito.sh
@@ -1,1 +1,11 @@
 mono CodeGenerator.exe Packet.proto
+
+mkdir CitoOutput
+mkdir CitoOutput/C
+mkdir CitoOutput/Java
+mkdir CitoOutput/Cs
+mkdir CitoOutput/JsTa
+
+cd CitoOutput/Java
+mono ../../CiTo.exe -D CITO -D JAVA -l java -o ManicDigger.java -n ManicDigger.lib  $(ls ../../ManicDiggerLib/Client/*.ci.cs) ../../Packet.Serializer.ci.cs
+cd ../..
