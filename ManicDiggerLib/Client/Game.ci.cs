manicdigger/manicdigger

commit 6b29e0552e83e9f5c801c4a61c6196b0d3a4e424

Refactoring: Mouse pointer lock.

ManicDiggerLib/Client/Game.ci.cs
@@ -1232,7 +1232,6 @@
     }
 
     internal GuiState guistate;
-    internal bool freemouse;
     internal bool overheadcamera;
     public bool GetFreeMouse()
     {
@@ -1240,19 +1239,25 @@
         {
             return true;
         }
-        return freemouse;
+        return !platform.IsMousePointerLocked();
     }
     public void SetFreeMouse(bool value)
     {
-        platform.SetFreeMouse(value);
-        freemouse = value;
+        if (value)
+        {
+            platform.ExitMousePointerLock();
+        }
+        else
+        {
+            platform.RequestMousePointerLock();
+        }
     }
     internal MapLoadingProgressEventArgs maploadingprogress;
 
     public void MapLoadingStart()
     {
         guistate = GuiState.MapLoading;
-        SetFreeMouse(true);
+        platform.ExitMousePointerLock();
         maploadingprogress = new MapLoadingProgressEventArgs();
         fontMapLoading = FontCi.Create("Arial", 14, 0);
     }
@@ -1560,7 +1565,7 @@
         if (type == CameraType.Fpp)
         {
             cameratype = CameraType.Fpp;
-            SetFreeMouse(false);
+            platform.RequestMousePointerLock();
             ENABLE_TPP_VIEW = false;
             overheadcamera = false;
         }
@@ -1573,7 +1578,7 @@
         {
             cameratype = CameraType.Overhead;
             overheadcamera = true;
-            SetFreeMouse(true);
+            platform.ExitMousePointerLock();
             ENABLE_TPP_VIEW = true;
             playerdestination = Vector3Ref.Create(player.playerposition.X, player.playerposition.Y, player.playerposition.Z);
         }
@@ -2351,7 +2356,6 @@
     internal int reloadstartMilliseconds;
     internal int PreviousActiveMaterialBlock;
     internal int lastOxygenTickMilliseconds;
-    internal bool freemousejustdisabled;
     internal int typinglogpos;
     internal TypingState GuiTyping;
     internal ConnectData connectdata;
@@ -3478,8 +3482,7 @@
     internal void GuiStateBackToGame()
     {
         guistate = GuiState.Normal;
-        SetFreeMouse(false);
-        freemousejustdisabled = true;
+        platform.RequestMousePointerLock();
     }
 
     internal float overheadcameradistance;
@@ -6127,7 +6130,7 @@
                 }
                 guistate = GuiState.EscapeMenu;
                 menustate = new MenuState();
-                SetFreeMouse(true);
+                platform.ExitMousePointerLock();
                 return;
             }
             if (eKey == GetKey(GlKeys.Number7) && IsShiftPressed && GuiTyping == TypingState.None) // don't need to hit enter for typing commands starting with slash
@@ -6283,14 +6286,14 @@
                 {
                     cameratype = CameraType.Overhead;
                     overheadcamera = true;
-                    SetFreeMouse(true);
+                    platform.ExitMousePointerLock();
                     ENABLE_TPP_VIEW = true;
                     playerdestination = Vector3Ref.Create(player.playerposition.X, player.playerposition.Y, player.playerposition.Z);
                 }
                 else if (cameratype == CameraType.Overhead)
                 {
                     cameratype = CameraType.Fpp;
-                    SetFreeMouse(false);
+                    platform.RequestMousePointerLock();
                     ENABLE_TPP_VIEW = false;
                     overheadcamera = false;
                 }
@@ -6434,7 +6437,7 @@
             {
                 guistate = GuiState.Inventory;
                 menustate = new MenuState();
-                SetFreeMouse(true);
+                platform.ExitMousePointerLock();
                 return;
             }
             HandleMaterialKeys(eKey);
@@ -6668,7 +6671,6 @@
 
     internal void FrameTick(float dt)
     {
-        UpdateMousePosition();
         //if ((DateTime.Now - lasttodo).TotalSeconds > BuildDelay && todo.Count > 0)
         //UpdateTerrain();
         OnNewFrame(dt);
@@ -7659,7 +7661,7 @@
         craftingTableposz = posz;
         guistate = GuiState.CraftingRecipes;
         menustate = new MenuState();
-        SetFreeMouse(true);
+        platform.ExitMousePointerLock();
     }
 
     internal int[] okrecipes;
@@ -7968,26 +7970,6 @@
 
     string lasthandimage;
 
-    internal void UpdateMousePosition()
-    {
-        UpdateMousePositionArgs args = new UpdateMousePositionArgs();
-        args.freemouse = GetFreeMouse();
-        args.freemousejustdisabled = freemousejustdisabled;
-        args.mouseCurrentX = mouseCurrentX;
-        args.mouseCurrentY = mouseCurrentY;
-        args.mouseDeltaX = mouseDeltaX;
-        args.mouseDeltaY = mouseDeltaY;
-
-        platform.UpdateMousePosition(args);
-
-        SetFreeMouse(args.freemouse);
-        freemousejustdisabled = args.freemousejustdisabled;
-        mouseCurrentX = args.mouseCurrentX;
-        mouseCurrentY = args.mouseCurrentY;
-        mouseDeltaX = args.mouseDeltaX;
-        mouseDeltaY = args.mouseDeltaY;
-    }
-
     bool startedconnecting;
     internal void GotoDraw2d(float dt)
     {
@@ -8012,6 +7994,30 @@
                 Connect__();
             }
         }
+    }
+
+    public void OnTouchStart(TouchEventArgs e)
+    {
+    }
+
+    public void OnTouchMove(TouchEventArgs e)
+    {
+    }
+
+    public void OnTouchEnd(TouchEventArgs e)
+    {
+    }
+
+    public void OnBackPressed()
+    {
+    }
+
+    public void MouseMove(MouseEventArgs e)
+    {
+        mouseCurrentX = e.GetX();
+        mouseCurrentY = e.GetY();
+        mouseDeltaX = e.GetMovementX();
+        mouseDeltaY = e.GetMovementY();
     }
 }
 

ManicDiggerLib/Client/EscapeMenu.ci.cs
@@ -304,7 +304,7 @@
     {
         game.guistate = GuiState.EscapeMenu;
         game.menustate = new MenuState();
-        game.SetFreeMouse(true);
+        game.platform.ExitMousePointerLock();
         SetEscapeMenuState(EscapeMenuState.Main);
     }
     EscapeMenuState escapemenustate;

ManicDiggerLib/Client/MainMenu.ci.cs
@@ -1299,6 +1299,15 @@ public class ScreenGame : Screen
         game.MouseDown(e);
     }
 
+    public override void OnMouseMove(MouseEventArgs e)
+    {
+        if (!game.platform.Focused())
+        {
+            return;
+        }
+        game.MouseMove(e);
+    }
+
     public override void OnMouseUp(MouseEventArgs e)
     {
         if (!game.platform.Focused())
@@ -1311,6 +1320,26 @@ public class ScreenGame : Screen
     public override void OnMouseWheel(MouseWheelEventArgs e)
     {
         game.MouseWheelChanged(e.GetDeltaPrecise());
+    }
+
+    public override void OnTouchStart(TouchEventArgs e)
+    {
+        game.OnTouchStart(e);
+    }
+
+    public override void OnTouchMove(TouchEventArgs e)
+    {
+        game.OnTouchMove(e);
+    }
+
+    public override void OnTouchEnd(TouchEventArgs e)
+    {
+        game.OnTouchEnd(e);
+    }
+
+    public override void OnBackPressed()
+    {
+        game.OnBackPressed();
     }
 }
 

ManicDiggerLib/Client/Platform.ci.cs
@@ -91,7 +91,6 @@
     public abstract AviWriterCi AviWriterCreate();
     public abstract bool StringEmpty(string data);
     public abstract float FloatModulo(float a, int b);
-    public abstract void SetFreeMouse(bool value);
     public abstract UriCi ParseUri(string uri);
     public abstract bool StringContains(string a, string b);
     public abstract RandomCi RandomCreate();
@@ -144,7 +143,6 @@
     public abstract void GlShadeModelSmooth();
     public abstract void MouseCursorHide();
     public abstract void ApplicationDoEvents();
-    public abstract void UpdateMousePosition(UpdateMousePositionArgs args);
     public abstract void ThreadSpinWait(int iterations);
     public abstract void GlDisableFog();
     public abstract bool SinglePlayerServerAvailable();
@@ -160,11 +158,13 @@
     public abstract bool IsFastSystem();
     public abstract Preferences GetPreferences();
     public abstract void SetPreferences(Preferences preferences);
+    public abstract bool IsMousePointerLocked();
+    public abstract void RequestMousePointerLock();
+    public abstract void ExitMousePointerLock();
 }
 
 public class UpdateMousePositionArgs
 {
-    internal bool freemouse;
     internal bool freemousejustdisabled;
     internal int mouseCurrentX;
     internal int mouseCurrentY;

ManicDiggerLib/Client/PlatformNative.cs
@@ -32,6 +32,8 @@ public class GamePlatformNative : GamePlatform
 
     public GameWindow window;
 
+    public bool TouchTest = false;
+
     string[] datapaths;
     Dictionary<string, string> cache = new Dictionary<string, string>();
     Dictionary<string, string> remap = new Dictionary<string, string>();
@@ -327,6 +329,18 @@ public class GamePlatformNative : GamePlatform
 
     void window_RenderFrame(object sender, OpenTK.FrameEventArgs e)
     {
+        UpdateMousePositionArgs a = new UpdateMousePositionArgs();
+        UpdateMousePosition(a);
+        foreach (MouseEventHandler h in mouseEventHandlers)
+        {
+            MouseEventArgs args2 = new MouseEventArgs();
+            args2.SetX(a.mouseCurrentX);
+            args2.SetY(a.mouseCurrentY);
+            args2.SetMovementX((int)a.mouseDeltaX);
+            args2.SetMovementY((int)a.mouseDeltaY);
+            h.OnMouseMove(args2);
+        }
+
         foreach (NewFrameHandler h in newFrameHandlers)
         {
             NewFrameEventArgs args = new NewFrameEventArgs();
@@ -349,36 +363,78 @@ public class GamePlatformNative : GamePlatform
 
     void Mouse_ButtonDown(object sender, MouseButtonEventArgs e)
     {
-        foreach (MouseEventHandler h in mouseEventHandlers)
+        if (TouchTest)
         {
-            MouseEventArgs args = new MouseEventArgs();
-            args.SetX(e.X);
-            args.SetY(e.Y);
-            args.SetButton((int)e.Button);
-            h.OnMouseDown(args);
+            foreach (TouchEventHandler h in touchEventHandlers)
+            {
+                TouchEventArgs args = new TouchEventArgs();
+                args.SetX(e.X);
+                args.SetY(e.Y);
+                args.SetId(0);
+                h.OnTouchStart(args);
+            }
+        }
+        else
+        {
+            foreach (MouseEventHandler h in mouseEventHandlers)
+            {
+                MouseEventArgs args = new MouseEventArgs();
+                args.SetX(e.X);
+                args.SetY(e.Y);
+                args.SetButton((int)e.Button);
+                h.OnMouseDown(args);
+            }
         }
     }
 
     void Mouse_ButtonUp(object sender, MouseButtonEventArgs e)
     {
-        foreach (MouseEventHandler h in mouseEventHandlers)
+        if (TouchTest)
         {
-            MouseEventArgs args = new MouseEventArgs();
-            args.SetX(e.X);
-            args.SetY(e.Y);
-            args.SetButton((int)e.Button);
-            h.OnMouseUp(args);
+            foreach (TouchEventHandler h in touchEventHandlers)
+            {
+                TouchEventArgs args = new TouchEventArgs();
+                args.SetX(e.X);
+                args.SetY(e.Y);
+                args.SetId(0);
+                h.OnTouchEnd(args);
+            }
+        }
+        else
+        {
+            foreach (MouseEventHandler h in mouseEventHandlers)
+            {
+                MouseEventArgs args = new MouseEventArgs();
+                args.SetX(e.X);
+                args.SetY(e.Y);
+                args.SetButton((int)e.Button);
+                h.OnMouseUp(args);
+            }
         }
     }
 
     void Mouse_Move(object sender, MouseMoveEventArgs e)
     {
-        foreach (MouseEventHandler h in mouseEventHandlers)
+        if (TouchTest)
         {
-            MouseEventArgs args = new MouseEventArgs();
-            args.SetX(e.X);
-            args.SetY(e.Y);
-            h.OnMouseMove(args);
+            foreach (TouchEventHandler h in touchEventHandlers)
+            {
+                TouchEventArgs args = new TouchEventArgs();
+                args.SetX(e.X);
+                args.SetY(e.Y);
+                args.SetId(0);
+                h.OnTouchMove(args);
+            }
+        }
+        else
+        {
+            foreach (MouseEventHandler h in mouseEventHandlers)
+            {
+                MouseEventArgs args = new MouseEventArgs();
+                args.SetX(e.X);
+                args.SetY(e.Y);
+                h.OnMouseMove(args);
+            }
         }
     }
 
@@ -1005,17 +1061,6 @@ public class GamePlatformNative : GamePlatform
         return a % b;
     }
 
-    public bool IsMac = Environment.OSVersion.Platform == PlatformID.MacOSX;
-
-    public override void SetFreeMouse(bool value)
-    {
-        if (IsMac)
-        {
-            window.CursorVisible = value;
-            System.Windows.Forms.Cursor.Hide();
-        }
-    }
-
     public override UriCi ParseUri(string uri)
     {
         MyUri myuri = new MyUri(uri);
@@ -1458,11 +1503,12 @@ public class GamePlatformNative : GamePlatform
         }
     }
 
-    public override void UpdateMousePosition(UpdateMousePositionArgs args)
+    bool wasMousePointerLocked;
+    void UpdateMousePosition(UpdateMousePositionArgs args)
     {
         args.mouseCurrentX = System.Windows.Forms.Cursor.Position.X;
         args.mouseCurrentY = System.Windows.Forms.Cursor.Position.Y;
-        if (args.freemouse)
+        if (!mousePointerLocked)
         {
             args.mouseCurrentX = args.mouseCurrentX - window.X;
             args.mouseCurrentY = args.mouseCurrentY - window.Y;
@@ -1473,13 +1519,14 @@ public class GamePlatformNative : GamePlatform
         {
             return;
         }
-        if (args.freemousejustdisabled)
+        if (mousePointerLocked && (!wasMousePointerLocked))
         {
             mouse_previous.X = args.mouseCurrentX;
             mouse_previous.Y = args.mouseCurrentY;
             args.freemousejustdisabled = false;
         }
-        if (!args.freemouse)
+        wasMousePointerLocked = mousePointerLocked;
+        if (mousePointerLocked)
         {
             //There are two versions:
 
@@ -1837,6 +1884,38 @@ public class GamePlatformNative : GamePlatform
         catch
         {
         }
+    }
+
+    bool mousePointerLocked;
+
+    public override bool IsMousePointerLocked()
+    {
+        return mousePointerLocked;
+    }
+
+    public bool IsMac = Environment.OSVersion.Platform == PlatformID.MacOSX;
+
+    public override void RequestMousePointerLock()
+    {
+        if (TouchTest)
+        {
+            return;
+        }
+        if (IsMac)
+        {
+            window.CursorVisible = false;
+            System.Windows.Forms.Cursor.Hide();
+        }
+        mousePointerLocked = true;
+    }
+
+    public override void ExitMousePointerLock()
+    {
+        if (IsMac)
+        {
+            window.CursorVisible = true;
+        }
+        mousePointerLocked = false;
     }
 }
 

commit a32a4260de925768b03df397248c5b9c44ff703c

Fixed FPP camera look on variable framerate. (broken in 6b29e0552)

ManicDiggerLib/Client/Game.ci.cs
@@ -2544,8 +2544,8 @@
     {
         if (!overheadcamera)
         {
-            player.playerorientation.Y += mouseDeltaX * rotationspeed * dt;
-            player.playerorientation.X += mouseDeltaY * rotationspeed * dt;
+            player.playerorientation.Y += mouseDeltaX * rotationspeed * (one / 75);
+            player.playerorientation.X += mouseDeltaY * rotationspeed * (one / 75);
             player.playerorientation.X = Game.ClampFloat(player.playerorientation.X,
                 Game.GetPi() / 2 + (one * 15 / 1000),
                 (Game.GetPi() / 2 + Game.GetPi() - (one * 15 / 1000)));
@@ -6675,10 +6675,6 @@
         //UpdateTerrain();
         OnNewFrame(dt);
         RailOnNewFrame(dt);
-        if (guistate == GuiState.Normal && enableCameraControl)
-        {
-            UpdateMouseViewportControl(dt);
-        }
         NetworkProcess();
 
         if (guistate == GuiState.MapLoading) { return; }
@@ -7839,7 +7835,7 @@
     internal void OnRenderFrame(float deltaTime)
     {
         UpdateResize();
-        terrainRenderer.UpdateTerrain();
+        
         if (guistate == GuiState.MapLoading)
         {
             platform.GlClearColorRgbaf(0, 0, 0, 1);
@@ -7848,6 +7844,12 @@
         {
             platform.GlClearColorRgbaf(one * Game.clearcolorR / 255, one * Game.clearcolorG / 255, one * Game.clearcolorB / 255, one * Game.clearcolorA / 255);
         }
+
+        if (guistate == GuiState.Normal && enableCameraControl)
+        {
+            UpdateMouseViewportControl(deltaTime);
+        }
+
         //Sleep is required in Mono for running the terrain background thread.
         platform.ApplicationDoEvents();
 
@@ -7953,6 +7955,7 @@
             }
         }
         GotoDraw2d(deltaTime);
+        terrainRenderer.UpdateTerrain();
     }
 
     int lastWidth;