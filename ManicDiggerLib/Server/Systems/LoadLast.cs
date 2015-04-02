05ee818e6872f2d5bae001746ddf857eebd47b0e
croxxx <i.am.croxxx@aol.de>
2015-04-02 13:52:08 +0200

    Server: Moved OnLoad() calls to ServerSystemLoadLast

--- ManicDiggerLib/Server/Systems/LoadLast.cs
+++ ManicDiggerLib/Server/Systems/LoadLast.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ManicDigger
 {
@@ -17,6 +18,34 @@ namespace ManicDigger
 		void LoadLastEvent(Server server)
 		{
 			// Add things that need to be done after all other systems here
+
+			// Mod OnLoad needs to be called after all other systems as some functions rely on stuff loaded after ModLoader
+			Call_ModOnLoad(server);
+		}
+
+		void Call_ModOnLoad(Server server)
+		{
+			// Create dictionary to hold Mod data if none has been loaded in savegame
+			if (server.moddata == null) { server.moddata = new Dictionary<string, byte[]>(); }
+			// Execute all methods registered using RegisterOnLoad(). This will fail if they contain errors.
+			for (int i = 0; i < server.onload.Count; i++)
+			{
+				server.onload[i]();
+			}
+			// Try to execute all methods registered using RegisterOnLoadWorld().
+			for (int i = 0; i < server.modEventHandlers.onloadworld.Count; i++)
+			{
+				try
+				{
+					server.modEventHandlers.onloadworld[i]();
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("Mod exception: OnLoadWorld");
+					Console.WriteLine(ex.Message);
+					Console.WriteLine(ex.StackTrace);
+				}
+			}
 		}
 	}
 }
