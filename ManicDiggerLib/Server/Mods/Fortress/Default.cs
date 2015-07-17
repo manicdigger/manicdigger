c5f784df121b92b928423dc31a41da8ade3a9bed
croxxx <i.am.croxxx@aol.de>
2015-07-17 22:04:15 +0200

    Added fallback Default.cs to prevent compatibility issues with old mods Proposed by @superstorm192, Fixes #179

---
+++ ManicDiggerLib/Server/Mods/Fortress/Default.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ManicDigger.Mods
+{
+    /// <summary>
+    /// This is a fix for backward compatibility issues of old Mods.
+    /// Do not reference this in your Mods. Instead reference the corresponding Core*.cs
+    /// </summary>
+    public class Default : IMod
+    {
+        public void PreStart(ModManager m)
+        {
+            m.RequireMod("Core");
+            m.RequireMod("CoreBlocks");
+            m.RequireMod("CoreCrafting");
+        }
+        public void Start(ModManager manager){ }
+    }
+}
