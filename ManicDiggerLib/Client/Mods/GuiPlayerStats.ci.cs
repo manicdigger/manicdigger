5bdaf7ab9702fc8a65454b7ca01bd1669855aaf0
croxxx <i.am.croxxx@aol.de>
2015-09-26 00:00:14 +0200

    Offset inner bars by 5 pixels

--- ManicDiggerLib/Client/Mods/GuiPlayerStats.ci.cs
+++ ManicDiggerLib/Client/Mods/GuiPlayerStats.ci.cs
@@ -15,9 +15,10 @@
     }
 
     //Sizes of Health/Oxygen bar
-    const int baseSizeX = 256;
+    const int baseSizeX = 220;
     const int baseSizeY = 32;
     const int centerOffset = 20;
+    const int barSpacing = 5;
 
     int healthPosX;
     int healthPosY;
@@ -30,7 +31,7 @@
         {
             float progress = game.one * game.PlayerStats.CurrentHealth / game.PlayerStats.MaxHealth;
             game.Draw2dTexture(game.WhiteTexture(), healthPosX, healthPosY, baseSizeX, baseSizeY, null, 0, Game.ColorFromArgb(255, 0, 0, 0), false);
-            game.Draw2dTexturePart(game.GetTexture("background.png"), progress, 1, healthPosX, healthPosY, (progress) * baseSizeX, baseSizeY, Game.ColorFromArgb(255, 255, 0, 0), false);
+            game.Draw2dTexturePart(game.GetTexture("background.png"), progress, 1, healthPosX + barSpacing, healthPosY + barSpacing, (progress) * (baseSizeX - (2 * barSpacing)), baseSizeY - (2 * barSpacing), Game.ColorFromArgb(255, 255, 0, 0), false);
         }
     }
 
@@ -42,7 +43,7 @@
             {
                 float progress = game.one * game.PlayerStats.CurrentOxygen / game.PlayerStats.MaxOxygen;
                 game.Draw2dTexture(game.WhiteTexture(), oxygenPosX, oxygenPosY, baseSizeX, baseSizeY, null, 0, Game.ColorFromArgb(255, 0, 0, 0), false);
-                game.Draw2dTexturePart(game.GetTexture("background.png"), progress, 1, oxygenPosX, oxygenPosY, (progress) * baseSizeX, baseSizeY, Game.ColorFromArgb(255, 0, 0, 255), false);
+                game.Draw2dTexturePart(game.GetTexture("background.png"), progress, 1, oxygenPosX + barSpacing, oxygenPosY + barSpacing, (progress) * (baseSizeX - (2 * barSpacing)), baseSizeY - (2 * barSpacing), Game.ColorFromArgb(255, 0, 0, 255), false);
             }
         }
     }
