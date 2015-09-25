061c20ac100872cd1bde51343876abaf644d142b
croxxx <i.am.croxxx@aol.de>
2015-09-26 01:40:32 +0200

    Use custom bar images

--- ManicDiggerLib/Client/Mods/GuiPlayerStats.ci.cs
+++ ManicDiggerLib/Client/Mods/GuiPlayerStats.ci.cs
@@ -3,9 +3,9 @@
     public override void OnNewFrameDraw2d(Game game, float deltaTime)
     {
         healthPosX = game.Width() / 2 - baseSizeX - centerOffset;
-        healthPosY = game.Height() - 150;
+        healthPosY = game.Height() - 115;
         oxygenPosX = game.Width() / 2 + centerOffset;
-        oxygenPosY = game.Height() - 150;
+        oxygenPosY = game.Height() - 115;
 
         if (game.guistate != GuiState.MapLoading)
         {
@@ -16,9 +16,9 @@
 
     //Sizes of Health/Oxygen bar
     const int baseSizeX = 220;
-    const int baseSizeY = 32;
+    const int baseSizeY = 16;
     const int centerOffset = 20;
-    const int barSpacing = 5;
+    const int barSpacing = 2;
 
     int healthPosX;
     int healthPosY;
@@ -30,8 +30,8 @@
         if (game.PlayerStats != null)
         {
             float progress = game.one * game.PlayerStats.CurrentHealth / game.PlayerStats.MaxHealth;
-            game.Draw2dTexture(game.WhiteTexture(), healthPosX, healthPosY, baseSizeX, baseSizeY, null, 0, Game.ColorFromArgb(255, 0, 0, 0), false);
-            game.Draw2dTexturePart(game.GetTexture("background.png"), progress, 1, healthPosX + barSpacing, healthPosY + barSpacing, (progress) * (baseSizeX - (2 * barSpacing)), baseSizeY - (2 * barSpacing), Game.ColorFromArgb(255, 255, 0, 0), false);
+            game.Draw2dTexture(game.GetTexture("ui_bar_background.png"), healthPosX, healthPosY, baseSizeX, baseSizeY, null, 0, Game.ColorFromArgb(255, 255, 255, 255), false);
+            game.Draw2dTexturePart(game.GetTexture("ui_bar_inner.png"), progress, 1, healthPosX + barSpacing, healthPosY + barSpacing, (progress) * (baseSizeX - (2 * barSpacing)), baseSizeY - (2 * barSpacing), Game.ColorFromArgb(255, 255, 0, 0), false);
         }
     }
 
@@ -42,8 +42,8 @@
             if (game.PlayerStats.CurrentOxygen < game.PlayerStats.MaxOxygen)
             {
                 float progress = game.one * game.PlayerStats.CurrentOxygen / game.PlayerStats.MaxOxygen;
-                game.Draw2dTexture(game.WhiteTexture(), oxygenPosX, oxygenPosY, baseSizeX, baseSizeY, null, 0, Game.ColorFromArgb(255, 0, 0, 0), false);
-                game.Draw2dTexturePart(game.GetTexture("background.png"), progress, 1, oxygenPosX + barSpacing, oxygenPosY + barSpacing, (progress) * (baseSizeX - (2 * barSpacing)), baseSizeY - (2 * barSpacing), Game.ColorFromArgb(255, 0, 0, 255), false);
+                game.Draw2dTexture(game.GetTexture("ui_bar_background.png"), oxygenPosX, oxygenPosY, baseSizeX, baseSizeY, null, 0, Game.ColorFromArgb(255, 255, 255, 255), false);
+                game.Draw2dTexturePart(game.GetTexture("ui_bar_inner.png"), progress, 1, oxygenPosX + barSpacing, oxygenPosY + barSpacing, (progress) * (baseSizeX - (2 * barSpacing)), baseSizeY - (2 * barSpacing), Game.ColorFromArgb(255, 0, 0, 255), false);
             }
         }
     }
