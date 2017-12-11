118acbd0d7f95acf42a72db52d7dbc836a26f7cd
croxxx <i.am.croxxx@aol.de>
2017-12-11 19:14:29 +0100

    Finish reworking ScreenMain

--- ManicDiggerLib/Client/UI/Screens/Main.ci.cs
+++ ManicDiggerLib/Client/UI/Screens/Main.ci.cs
@@ -1,144 +1,160 @@
 ﻿public class ScreenMain : Screen
 {
-    public ScreenMain()
-    {
-		logo = new ImageWidget();
-		logo.SetTextureName("logo.png");
-        singleplayer = new ButtonWidget();
-		singleplayer.SetClickable(true);
-        multiplayer = new ButtonWidget();
-		multiplayer.SetClickable(true);
-		exit = new ButtonWidget();
-		exit.SetClickable(true);
-		AddWidgetNew(logo);
-		AddWidgetNew(singleplayer);
-		AddWidgetNew(multiplayer);
-		AddWidgetNew(exit);
-        queryStringChecked = false;
-        cursorLoaded = false;
-        fontDefault = new FontCi();
-        fontDefault.size = 20;
-    }
+	public ScreenMain()
+	{
+		queryStringChecked = false;
+		cursorLoaded = false;
+		assetsLoaded = false;
 
-	ImageWidget logo;
-    ButtonWidget singleplayer;
-	ButtonWidget multiplayer;
-	ButtonWidget exit;
-    internal float windowX;
-    internal float windowY;
-    bool queryStringChecked;
-    bool cursorLoaded;
-    FontCi fontDefault;
+		wtxt_loading = new TextWidget();
+		FontCi fontDefault = new FontCi();
+		fontDefault.size = 20;
+		wtxt_loading.SetFont(fontDefault);
+		wtxt_loading.SetAlignment(TextAlign.Center);
+		wtxt_loading.SetBaseline(TextBaseline.Middle);
+		AddWidgetNew(wtxt_loading);
 
-    public override void Render(float dt)
-    {
-        windowX = menu.p.GetCanvasWidth();
-        windowY = menu.p.GetCanvasHeight();
+		wimg_logo = new ImageWidget();
+		wimg_logo.SetTextureName("logo.png");
+		AddWidgetNew(wimg_logo);
 
-        float scale = menu.GetScale();
+		wbtn_singleplayer = new ButtonWidget();
+		wbtn_singleplayer.SetClickable(true);
+		AddWidgetNew(wbtn_singleplayer);
 
-        if (menu.assetsLoadProgress.value != 1)
-        {
-            string s = menu.p.StringFormat(menu.lang.Get("MainMenu_AssetsLoadProgress"), menu.p.FloatToString(menu.p.FloatToInt(menu.assetsLoadProgress.value * 100)));
-            menu.DrawText(s, fontDefault, windowX / 2, windowY / 2, TextAlign.Center, TextBaseline.Middle);
-            return;
-        }
+		wbtn_multiplayer = new ButtonWidget();
+		wbtn_multiplayer.SetClickable(true);
+		AddWidgetNew(wbtn_multiplayer);
 
-        if (!cursorLoaded)
-        {
-            menu.p.SetWindowCursor(0, 0, 32, 32, menu.GetFile("mousecursor.png"), menu.GetFileLength("mousecursor.png"));
-            cursorLoaded = true;
-        }
+		wbtn_exit = new ButtonWidget();
+		wbtn_exit.SetClickable(true);
+		AddWidgetNew(wbtn_exit);
+	}
 
-        UseQueryStringIpAndPort(menu);
+	TextWidget wtxt_loading;
+	ImageWidget wimg_logo;
+	ButtonWidget wbtn_singleplayer;
+	ButtonWidget wbtn_multiplayer;
+	ButtonWidget wbtn_exit;
+	internal float windowX;
+	internal float windowY;
+	bool queryStringChecked;
+	bool assetsLoaded;
+	bool cursorLoaded;
 
-        menu.DrawBackground();
-		//menu.Draw2dQuad(menu.GetTexture("logo.png"), windowX / 2 - 1024 * scale / 2, 0, 1024 * scale, 512 * scale);
+	public override void Render(float dt)
+	{
+		windowX = menu.p.GetCanvasWidth();
+		windowY = menu.p.GetCanvasHeight();
 
-		logo.sizex = 1024 * scale;
-		logo.sizey = 256 * scale;
-		logo.x = windowX / 2 - logo.sizex / 2;
-		logo.y = 50 * scale;
+		if (!assetsLoaded)
+		{
+			if (menu.assetsLoadProgress.value != 1)
+			{
+				string s = menu.p.StringFormat(menu.lang.Get("MainMenu_AssetsLoadProgress"), menu.p.FloatToString(menu.p.FloatToInt(menu.assetsLoadProgress.value * 100)));
+				wtxt_loading.SetX(windowX / 2);
+				wtxt_loading.SetY(windowY / 2);
+				wtxt_loading.SetText(s);
+				return;
+			}
+			assetsLoaded = true;
+			wtxt_loading.SetVisible(false);
+		}
 
+		if (!cursorLoaded)
+		{
+			menu.p.SetWindowCursor(0, 0, 32, 32, menu.GetFile("mousecursor.png"), menu.GetFileLength("mousecursor.png"));
+			cursorLoaded = true;
+		}
+
+		if (!queryStringChecked)
+		{
+			UseQueryStringIpAndPort(menu);
+			queryStringChecked = true;
+		}
+
+		menu.DrawBackground();
+
+		float scale = menu.GetScale();
 		float buttonheight = 64 * scale;
 		float buttonwidth = 256 * scale;
 		float spacebetween = 5 * scale;
 		float offsetfromborder = 50 * scale;
 
-        singleplayer.SetText(menu.lang.Get("MainMenu_Singleplayer"));
-        singleplayer.x = windowX / 2 - (buttonwidth / 2);
-        singleplayer.y = windowY - (3 * (buttonheight + spacebetween)) - offsetfromborder;
-        singleplayer.sizex = buttonwidth;
-        singleplayer.sizey = buttonheight;
+		wimg_logo.sizex = 1024 * scale;
+		wimg_logo.sizey = 256 * scale;
+		wimg_logo.x = windowX / 2 - wimg_logo.sizex / 2;
+		wimg_logo.y = 50 * scale;
 
-        multiplayer.SetText(menu.lang.Get("MainMenu_Multiplayer"));
-        multiplayer.x = windowX / 2 - (buttonwidth / 2);
-        multiplayer.y = windowY - (2 * (buttonheight + spacebetween)) - offsetfromborder;
-        multiplayer.sizex = buttonwidth;
-        multiplayer.sizey = buttonheight;
+		wbtn_singleplayer.SetText(menu.lang.Get("MainMenu_Singleplayer"));
+		wbtn_singleplayer.x = windowX / 2 - (buttonwidth / 2);
+		wbtn_singleplayer.y = windowY - (3 * (buttonheight + spacebetween)) - offsetfromborder;
+		wbtn_singleplayer.sizex = buttonwidth;
+		wbtn_singleplayer.sizey = buttonheight;
 
-        exit.visible = menu.p.ExitAvailable();
+		wbtn_multiplayer.SetText(menu.lang.Get("MainMenu_Multiplayer"));
+		wbtn_multiplayer.x = windowX / 2 - (buttonwidth / 2);
+		wbtn_multiplayer.y = windowY - (2 * (buttonheight + spacebetween)) - offsetfromborder;
+		wbtn_multiplayer.sizex = buttonwidth;
+		wbtn_multiplayer.sizey = buttonheight;
 
-        exit.SetText(menu.lang.Get("MainMenu_Quit"));
-        exit.x = windowX / 2 - (buttonwidth / 2);
-        exit.y = windowY - (1 * (buttonheight + spacebetween)) - offsetfromborder;
-        exit.sizex = buttonwidth;
-        exit.sizey = buttonheight;
-        DrawWidgets();
-    }
+		wbtn_exit.visible = menu.p.ExitAvailable();
+		wbtn_exit.SetText(menu.lang.Get("MainMenu_Quit"));
+		wbtn_exit.x = windowX / 2 - (buttonwidth / 2);
+		wbtn_exit.y = windowY - (1 * (buttonheight + spacebetween)) - offsetfromborder;
+		wbtn_exit.sizex = buttonwidth;
+		wbtn_exit.sizey = buttonheight;
 
-    void UseQueryStringIpAndPort(MainMenu menu)
-    {
-        if (queryStringChecked)
-        {
-            return;
-        }
-        queryStringChecked = true;
-        string ip = menu.p.QueryStringValue("ip");
-        string port = menu.p.QueryStringValue("port");
-        int portInt = 25565;
-        if (port != null && menu.p.FloatTryParse(port, new FloatRef()))
-        {
-            portInt = menu.p.IntParse(port);
-        }
-        if (ip != null)
-        {
-            menu.StartLogin(null, ip, portInt);
-        }
-    }
+		DrawWidgets();
+	}
 
-    public override void OnButtonA(AbstractMenuWidget w)
-    {
-		if (w == singleplayer)
+	public override void OnButtonA(AbstractMenuWidget w)
+	{
+		if (w == wbtn_singleplayer)
 		{
 			menu.StartSingleplayer();
 		}
-		if (w == multiplayer)
+		if (w == wbtn_multiplayer)
 		{
 			menu.StartMultiplayer();
 		}
-		if (w == exit)
+		if (w == wbtn_exit)
 		{
 			menu.Exit();
 		}
 	}
 
-    public override void OnBackPressed()
-    {
-        menu.Exit();
-    }
+	public override void OnBackPressed()
+	{
+		menu.Exit();
+	}
 
-    public override void OnKeyDown(KeyEventArgs e)
-    {
-        // debug
-        if (e.GetKeyCode() == GlKeys.F5)
-        {
-            menu.p.SinglePlayerServerDisable();
-            menu.StartGame(true, menu.p.PathCombine(menu.p.PathSavegames(), "Default.mdss"), null);
-        }
-        if (e.GetKeyCode() == GlKeys.F6)
-        {
-            menu.StartGame(true, menu.p.PathCombine(menu.p.PathSavegames(), "Default.mddbs"), null);
-        }
-    }
+	public override void OnKeyDown(KeyEventArgs e)
+	{
+		// debug
+		if (e.GetKeyCode() == GlKeys.F5)
+		{
+			menu.p.SinglePlayerServerDisable();
+			menu.StartGame(true, menu.p.PathCombine(menu.p.PathSavegames(), "Default.mdss"), null);
+		}
+		if (e.GetKeyCode() == GlKeys.F6)
+		{
+			menu.StartGame(true, menu.p.PathCombine(menu.p.PathSavegames(), "Default.mddbs"), null);
+		}
+	}
+
+	void UseQueryStringIpAndPort(MainMenu menu)
+	{
+		string ip = menu.p.QueryStringValue("ip");
+		string port = menu.p.QueryStringValue("port");
+		int portInt = 25565;
+		if (port != null && menu.p.FloatTryParse(port, new FloatRef()))
+		{
+			portInt = menu.p.IntParse(port);
+		}
+		if (ip != null)
+		{
+			menu.StartLogin(null, ip, portInt);
+		}
+	}
 }
