d322f8dc7e51585320cb2cd241d0d56b169caa4d
croxxx <i.am.croxxx@aol.de>
2018-04-06 21:59:15 +0200

    ScreenSingleplayer: Migrate to new widget system

--- ManicDiggerLib/Client/UI/Screens/Singleplayer.ci.cs
+++ ManicDiggerLib/Client/UI/Screens/Singleplayer.ci.cs
@@ -1,189 +1,185 @@
 ﻿public class ScreenSingleplayer : Screen
 {
-    public ScreenSingleplayer()
-    {
-        play = new MenuWidget();
-        play.text = "Play";
-        newWorld = new MenuWidget();
-        newWorld.text = "New World";
-        modify = new MenuWidget();
-        modify.text = "Modify";
-        back = new MenuWidget();
-        back.text = "Back";
-        back.type = WidgetType.Button;
-        open = new MenuWidget();
-        open.text = "Create or open...";
-        open.type = WidgetType.Button;
+	public ScreenSingleplayer()
+	{
+		// initialize widgets
+		wbtn_newWorld = new ButtonWidget();
+		wbtn_newWorld.SetText("New World");
+		wbtn_newWorld.SetVisible(false);
+		AddWidgetNew(wbtn_newWorld);
+		wbtn_playWorld = new ButtonWidget();
+		wbtn_playWorld.SetText("Play");
+		wbtn_playWorld.SetVisible(false);
+		AddWidgetNew(wbtn_playWorld);
+		wbtn_modifyWorld = new ButtonWidget();
+		wbtn_modifyWorld.SetText("Modify");
+		wbtn_modifyWorld.SetVisible(false);
+		AddWidgetNew(wbtn_modifyWorld);
+		wbtn_back = new ButtonWidget();
+		wbtn_back.SetText("Back");
+		AddWidgetNew(wbtn_back);
+		wbtn_openFile = new ButtonWidget();
+		wbtn_openFile.SetText("Create or open...");
+		AddWidgetNew(wbtn_openFile);
 
-        title = "Singleplayer";
-        
-        fontDefault = new FontCi();
-        fontDefault.size = 16;
+		wtxt_title = new TextWidget();
+		wtxt_title.SetFont(fontTitle);
+		wtxt_title.SetText("Singleplayer");
+		AddWidgetNew(wtxt_title);
+		wtxt_singleplayerUnavailable = new TextWidget();
+		wtxt_singleplayerUnavailable.SetFont(fontDefault);
+		wtxt_singleplayerUnavailable.SetText("Singleplayer is only available on desktop (Windows, Linux, Mac) version of game.");
+		wtxt_singleplayerUnavailable.SetVisible(false);
+		AddWidgetNew(wtxt_singleplayerUnavailable);
 
-		AddWidget(play);
-		AddWidget(newWorld);
-		AddWidget(modify);
-		AddWidget(back);
-		AddWidget(open);
+		worldButtons = new ButtonWidget[10];
+		for (int i = 0; i < 10; i++)
+		{
+			worldButtons[i] = new ButtonWidget();
+			worldButtons[i].SetVisible(false);
+			AddWidgetNew(worldButtons[i]);
+		}
+	}
 
-        worldButtons = new MenuWidget[10];
-        for (int i = 0; i < 10; i++)
-        {
-            worldButtons[i] = new MenuWidget();
-            worldButtons[i].visible = false;
-            widgets[5 + i] = worldButtons[i];
-        }
-    }
+	ButtonWidget wbtn_newWorld;
+	ButtonWidget wbtn_playWorld;
+	ButtonWidget wbtn_modifyWorld;
+	ButtonWidget wbtn_back;
+	ButtonWidget wbtn_openFile;
+	TextWidget wtxt_title;
+	TextWidget wtxt_singleplayerUnavailable;
 
-    MenuWidget newWorld;
-    MenuWidget play;
-    MenuWidget modify;
-    MenuWidget back;
-    MenuWidget open;
+	ButtonWidget[] worldButtons;
 
-    MenuWidget[] worldButtons;
+	string[] savegames;
+	int savegamesCount;
 
-    string[] savegames;
-    int savegamesCount;
-    string title;
-    FontCi fontDefault;
+	public override void LoadTranslations()
+	{
+		wbtn_back.SetText(menu.lang.Get("MainMenu_ButtonBack"));
+		wbtn_openFile.SetText(menu.lang.Get("MainMenu_SingleplayerButtonCreate"));
+		wtxt_title.SetText(menu.lang.Get("MainMenu_Singleplayer"));
+	}
 
-    public override void LoadTranslations()
-    {
-        back.text = menu.lang.Get("MainMenu_ButtonBack");
-        open.text = menu.lang.Get("MainMenu_SingleplayerButtonCreate");
-        title = menu.lang.Get("MainMenu_Singleplayer");
-    }
+	public override void Render(float dt)
+	{
+		GamePlatform p = menu.p;
+		float scale = menu.GetScale();
+		float leftx = p.GetCanvasWidth() / 2 - 128 * scale;
+		float y = p.GetCanvasHeight() / 2 + 0 * scale;
 
-    public override void Render(float dt)
-    {
-        GamePlatform p = menu.p;
+		wbtn_playWorld.x = leftx;
+		wbtn_playWorld.y = y + 100 * scale;
+		wbtn_playWorld.sizex = 256 * scale;
+		wbtn_playWorld.sizey = 64 * scale;
 
-        float scale = menu.GetScale();
+		wbtn_newWorld.x = leftx;
+		wbtn_newWorld.y = y + 170 * scale;
+		wbtn_newWorld.sizex = 256 * scale;
+		wbtn_newWorld.sizey = 64 * scale;
 
-        menu.DrawBackground();
-        menu.DrawText(title, menu.fontMenuHeading, p.GetCanvasWidth() / 2, 10, TextAlign.Center, TextBaseline.Top);
+		wbtn_modifyWorld.x = leftx;
+		wbtn_modifyWorld.y = y + 240 * scale;
+		wbtn_modifyWorld.sizex = 256 * scale;
+		wbtn_modifyWorld.sizey = 64 * scale;
 
-        float leftx = p.GetCanvasWidth() / 2 - 128 * scale;
-        float y = p.GetCanvasHeight() / 2 + 0 * scale;
+		wbtn_back.x = 40 * scale;
+		wbtn_back.y = p.GetCanvasHeight() - 104 * scale;
+		wbtn_back.sizex = 256 * scale;
+		wbtn_back.sizey = 64 * scale;
 
-        play.x = leftx;
-        play.y = y + 100 * scale;
-        play.sizex = 256 * scale;
-        play.sizey = 64 * scale;
+		wbtn_openFile.x = leftx;
+		wbtn_openFile.y = y + 0 * scale;
+		wbtn_openFile.sizex = 256 * scale;
+		wbtn_openFile.sizey = 64 * scale;
 
-        newWorld.x = leftx;
-        newWorld.y = y + 170 * scale;
-        newWorld.sizex = 256 * scale;
-        newWorld.sizey = 64 * scale;
+		wtxt_title.x = p.GetCanvasWidth() / 2;
+		wtxt_title.y = 10;
+		wtxt_title.SetAlignment(TextAlign.Center);
 
-        modify.x = leftx;
-        modify.y = y + 240 * scale;
-        modify.sizex = 256 * scale;
-        modify.sizey = 64 * scale;
+		// TODO: Implement savegame handling in game menu
+		//UpdateSavegameList();
+		for (int i = 0; i < 10; i++)
+		{
+			worldButtons[i].visible = false;
+		}
+		for (int i = 0; i < savegamesCount; i++)
+		{
+			worldButtons[i].SetVisible(true);
+			worldButtons[i].SetText(menu.p.FileName(savegames[i]));
+			worldButtons[i].x = leftx;
+			worldButtons[i].y = 100 + 100 * scale * i;
+			worldButtons[i].sizex = 256 * scale;
+			worldButtons[i].sizey = 64 * scale;
+		}
 
-        back.x = 40 * scale;
-        back.y = p.GetCanvasHeight() - 104 * scale;
-        back.sizex = 256 * scale;
-        back.sizey = 64 * scale;
+		menu.DrawBackground();
+		DrawWidgets();
 
-        open.x = leftx;
-        open.y = y + 0 * scale;
-        open.sizex = 256 * scale;
-        open.sizey = 64 * scale;
+		if (!menu.p.SinglePlayerServerAvailable())
+		{
+			wbtn_openFile.SetVisible(false);
+			wtxt_singleplayerUnavailable.SetVisible(true);
+			wtxt_singleplayerUnavailable.x = p.GetCanvasWidth() / 2;
+			wtxt_singleplayerUnavailable.y = p.GetCanvasHeight() / 2;
+			wtxt_singleplayerUnavailable.SetAlignment(TextAlign.Center);
+			wtxt_singleplayerUnavailable.SetBaseline(TextBaseline.Middle);
+		}
+	}
 
-        if (savegames == null)
-        {
-            IntRef savegamesCount_ = new IntRef();
-            savegames = menu.GetSavegames(savegamesCount_);
-            savegamesCount = savegamesCount_.value;
-        }
+	public override void OnBackPressed()
+	{
+		menu.StartMainMenu();
+	}
 
-        for (int i = 0; i < 10; i++)
-        {
-            worldButtons[i].visible = false;
-        }
-        for (int i = 0; i < savegamesCount; i++)
-        {
-            worldButtons[i].visible = true;
-            worldButtons[i].text = menu.p.FileName(savegames[i]);
-            worldButtons[i].x = leftx;
-            worldButtons[i].y = 100 + 100 * scale * i;
-            worldButtons[i].sizex = 256 * scale;
-            worldButtons[i].sizey = 64 * scale;
-        }
+	public override void OnButtonA(AbstractMenuWidget w)
+	{
+		if (w == wbtn_newWorld)
+		{
+			menu.StartNewWorld();
+		}
 
-        open.visible = menu.p.SinglePlayerServerAvailable();
-        play.visible = false;
-        newWorld.visible = false;
-        modify.visible = false;
-        for (int i = 0; i < savegamesCount; i++)
-        {
-            worldButtons[i].visible = false;
-        }
+		if (w == wbtn_playWorld)
+		{
 
-        DrawWidgets();
+		}
 
-        if (!menu.p.SinglePlayerServerAvailable())
-        {
-            menu.DrawText("Singleplayer is only available on desktop (Windows, Linux, Mac) version of game.", fontDefault, menu.p.GetCanvasWidth() / 2, menu.p.GetCanvasHeight() / 2, TextAlign.Center, TextBaseline.Middle);
-        }
-    }
+		if (w == wbtn_modifyWorld)
+		{
+			menu.StartModifyWorld();
+		}
 
-    public override void OnBackPressed()
-    {
-        menu.StartMainMenu();
-    }
+		if (w == wbtn_back)
+		{
+			OnBackPressed();
+		}
 
-    public override void OnButton(MenuWidget w)
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            worldButtons[i].selected = false;
-        }
-        for (int i = 0; i < 10; i++)
-        {
-            if (worldButtons[i] == w)
-            {
-                worldButtons[i].selected = true;
-            }
-        }
+		if (w == wbtn_openFile)
+		{
+			string extension;
+			if (menu.p.SinglePlayerServerAvailable())
+			{
+				extension = "mddbs";
+			}
+			else
+			{
+				extension = "mdss";
+			}
+			string result = menu.p.FileOpenDialog(extension, "Manic Digger Savegame", menu.p.PathSavegames());
+			if (result != null)
+			{
+				menu.ConnectToSingleplayer(result);
+			}
+		}
+	}
 
-        if (w == newWorld)
-        {
-            menu.StartNewWorld();
-        }
-
-        if (w == play)
-        {
-        }
-
-        if (w == modify)
-        {
-            menu.StartModifyWorld();
-        }
-
-        if (w == back)
-        {
-            OnBackPressed();
-        }
-
-        if (w == open)
-        {
-            string extension;
-            if (menu.p.SinglePlayerServerAvailable())
-            {
-                extension = "mddbs";
-            }
-            else
-            {
-                extension = "mdss";
-            }
-            string result = menu.p.FileOpenDialog(extension, "Manic Digger Savegame", menu.p.PathSavegames());
-            if (result != null)
-            {
-                menu.ConnectToSingleplayer(result);
-            }
-        }
-    }
+	void UpdateSavegameList()
+	{
+		if (savegames == null)
+		{
+			IntRef savegamesCount_ = new IntRef();
+			savegames = menu.GetSavegames(savegamesCount_);
+			savegamesCount = savegamesCount_.value;
+		}
+	}
 }
