95dadb519365142e8aa42eef69523d5588548c3d
croxxx <i.am.croxxx@aol.de>
2018-04-07 02:36:16 +0200

    ListWidget: implement scrolling

--- ManicDiggerLib/Client/UI/Widgets/ListWidget.ci.cs
+++ ManicDiggerLib/Client/UI/Widgets/ListWidget.ci.cs
@@ -1,6 +1,9 @@
 ﻿class ListWidget : AbstractMenuWidget
 {
 	ServerButtonWidget[] listButtons;
+	ButtonWidget wbtn_pageUp;
+	ButtonWidget wbtn_pageDown;
+	TextWidget wtxt_pageNr;
 	ListEntry[] listEntries;
 	const int entriesMax = 1024;
 	int entriesCount;
@@ -16,6 +19,17 @@
 		entriesCount = 0;
 		entriesPerPage = 0;
 		currentPage = 0;
+
+		wbtn_pageUp = new ButtonWidget();
+		wbtn_pageUp.SetVisible(false);
+		wbtn_pageDown = new ButtonWidget();
+		wbtn_pageDown.SetVisible(false);
+		FontCi fontPageNr = new FontCi();
+		wtxt_pageNr = new TextWidget();
+		wtxt_pageNr.SetAlignment(TextAlign.Right);
+		wtxt_pageNr.SetBaseline(TextBaseline.Middle);
+		wtxt_pageNr.SetFont(fontPageNr);
+		wtxt_pageNr.SetVisible(false);
 	}
 
 	public override void Draw(MainMenu m)
@@ -37,6 +51,7 @@
 				b.SetVisible(false);
 				listButtons[i] = b;
 			}
+			UpdateScrollButtons();
 			lastSizeY = sizey;
 		}
 
@@ -74,13 +89,35 @@
 			listButtons[i].SetVisible(true);
 		}
 
+		wbtn_pageUp.x = x + sizex - elementSizeY * scale;
+		wbtn_pageUp.y = y + (entriesPerPage - 1) * (elementSizeY + padding) * scale;
+		wbtn_pageUp.sizex = 64 * scale;
+		wbtn_pageUp.sizey = 64 * scale;
+		string texName = "serverlist_nav_down.png";
+		wbtn_pageUp.SetTextureNames(texName, texName, texName);
+
+		wbtn_pageDown.x = x + sizex - elementSizeY * scale;
+		wbtn_pageDown.y = y;
+		wbtn_pageDown.sizex = 64 * scale;
+		wbtn_pageDown.sizey = 64 * scale;
+		texName = "serverlist_nav_up.png";
+		wbtn_pageDown.SetTextureNames(texName, texName, texName);
+
+		wtxt_pageNr.SetText(m.p.IntToString(currentPage + 1));
+		wtxt_pageNr.x = x + sizex - (elementSizeY / 2) * scale;
+		wtxt_pageNr.y = y + (sizey / 2);
+		wtxt_pageNr.SetAlignment(TextAlign.Center);
+		wtxt_pageNr.SetBaseline(TextBaseline.Middle);
+
 		// draw child elements
 		for (int i = 0; i < entriesPerPage; i++)
 		{
 			listButtons[i].Draw(m);
 		}
+		wbtn_pageUp.Draw(m);
+		wbtn_pageDown.Draw(m);
+		wtxt_pageNr.Draw(m);
 	}
-
 	public override void OnMouseDown(GamePlatform p, MouseEventArgs args)
 	{
 		if (!HasBeenClicked(args)) { return; }
@@ -88,8 +125,58 @@
 		{
 			listButtons[i].OnMouseDown(p, args);
 		}
+		wbtn_pageUp.OnMouseDown(p, args);
+		if (wbtn_pageUp.HasBeenClicked(args))
+		{
+			PageUp();
+		}
+		wbtn_pageDown.OnMouseDown(p, args);
+		if (wbtn_pageDown.HasBeenClicked(args))
+		{
+			PageDown();
+		}
 	}
-	
+	public override void OnMouseWheel(GamePlatform p, MouseWheelEventArgs e)
+	{
+		if (e.GetDelta() < 0)
+		{
+			PageUp();
+		}
+		else if (e.GetDelta() > 0)
+		{
+			PageDown();
+		}
+	}
+
+	void PageUp()
+	{
+		if (!IsLastPage())
+		{
+			currentPage++;
+			UpdateScrollButtons();
+		}
+	}
+	void PageDown()
+	{
+		if (currentPage > 0)
+		{
+			currentPage--;
+			UpdateScrollButtons();
+		}
+	}
+	bool IsLastPage()
+	{
+		// determine if page is the last containing servers
+		return ((currentPage + 1) * entriesPerPage >= entriesMax) ||
+			(listEntries[(currentPage + 1) * entriesPerPage] == null);
+	}
+	public void UpdateScrollButtons()
+	{
+		// hide scroll buttons
+		wbtn_pageDown.SetVisible((currentPage == 0) ? false : true);
+		wbtn_pageUp.SetVisible(IsLastPage() ? false : true);
+	}
+
 	public void AddElement(ListEntry newEntry)
 	{
 		if (entriesCount < entriesMax)
@@ -97,12 +184,25 @@
 			listEntries[entriesCount] = newEntry;
 			entriesCount++;
 		}
+		UpdateScrollButtons();
+	}
+
+	public ListEntry GetElement(int index)
+	{
+		if (index < 0 ||
+			index >= entriesCount ||
+			listEntries == null)
+		{
+			return null;
+		}
+		return listEntries[index];
 	}
 
 	public void Clear()
 	{
 		listEntries = new ListEntry[entriesMax];
 		entriesCount = 0;
+		UpdateScrollButtons();
 	}
 
 	public int GetIndexSelected()
