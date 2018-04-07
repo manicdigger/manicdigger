577b750b8254508380de993df0603736419cc955
croxxx <i.am.croxxx@aol.de>
2018-04-07 16:28:40 +0200

    ScreenLogin: Use new widget system

--- ManicDiggerLib/Client/UI/Screens/Login.ci.cs
+++ ManicDiggerLib/Client/UI/Screens/Login.ci.cs
@@ -1,269 +1,233 @@
 ﻿public class ScreenLogin : Screen
 {
-    public ScreenLogin()
-    {
-        login = new MenuWidget();
-        login.text = "Login";
-        login.type = WidgetType.Button;
-        login.nextWidget = 9;
-        loginUsername = new MenuWidget();
-        loginUsername.type = WidgetType.Textbox;
-        loginUsername.text = "";
-        loginUsername.description = "Username";
-        loginUsername.nextWidget = 2;
-        loginPassword = new MenuWidget();
-        loginPassword.type = WidgetType.Textbox;
-        loginPassword.text = "";
-        loginPassword.description = "Password";
-        loginPassword.password = true;
-        loginPassword.nextWidget = 3;
-        loginRememberMe = new MenuWidget();
-        loginRememberMe.text = "Yes";
-        loginRememberMe.type = WidgetType.Button;
-        loginRememberMe.description = "Remember me";
-        loginRememberMe.nextWidget = 0;
+	public ScreenLogin()
+	{
+		wbtn_back = new ButtonWidget();
+		wbtn_back.SetText("Back");
+		AddWidgetNew(wbtn_back);
+		wbtn_login = new ButtonWidget();
+		wbtn_login.SetText("Login");
+		AddWidgetNew(wbtn_login);
+		wbtn_createAccount = new ButtonWidget();
+		wbtn_createAccount.SetText("Create Account");
+		wbtn_createAccount.SetVisible(false);
+		AddWidgetNew(wbtn_createAccount);
+		wtbx_username = new TextBoxWidget();
+		AddWidgetNew(wtbx_username);
+		wtbx_password = new TextBoxWidget();
+		wtbx_password.SetInputHidden(true);
+		AddWidgetNew(wtbx_password);
+		wcbx_rememberPassword = new CheckBoxWidget();
+		wcbx_rememberPassword.SetDescription("Remember me");
+		AddWidgetNew(wcbx_rememberPassword);
+		wtxt_title = new TextWidget();
+		wtxt_title.SetFont(fontTitle);
+		wtxt_title.SetText("Login");
+		AddWidgetNew(wtxt_title);
+		wtxt_statusMessage = new TextWidget();
+		wtxt_statusMessage.SetFont(fontDefault);
+		wtxt_statusMessage.SetAlignment(TextAlign.Center);
+		wtxt_statusMessage.SetVisible(false);
+		AddWidgetNew(wtxt_statusMessage);
+		wtxt_username = new TextWidget();
+		wtxt_username.SetFont(fontDefault);
+		wtxt_username.SetAlignment(TextAlign.Right);
+		wtxt_username.SetBaseline(TextBaseline.Middle);
+		wtxt_username.SetText("Username");
+		AddWidgetNew(wtxt_username);
+		wtxt_password = new TextWidget();
+		wtxt_password.SetFont(fontDefault);
+		wtxt_password.SetAlignment(TextAlign.Right);
+		wtxt_password.SetBaseline(TextBaseline.Middle);
+		wtxt_password.SetText("Password");
+		AddWidgetNew(wtxt_password);
 
-        createAccount = new MenuWidget();
-        createAccount.text = "Create account";
-        createAccount.type = WidgetType.Button;
-        createAccountUsername = new MenuWidget();
-        createAccountUsername.text = "";
-        createAccountUsername.type = WidgetType.Textbox;
-        createAccountUsername.description = "Username";
-        createAccountPassword = new MenuWidget();
-        createAccountPassword.text = "";
-        createAccountPassword.type = WidgetType.Textbox;
-        createAccountPassword.description = "Password";
-        createAccountPassword.password = true;
-        createAccountRememberMe = new MenuWidget();
-        createAccountRememberMe.text = "Yes";
-        createAccountRememberMe.type = WidgetType.Button;
-        createAccountRememberMe.description = "Remember me";
-        back = new MenuWidget();
-        back.text = "Back";
-        back.type = WidgetType.Button;
-        back.nextWidget = 1;
-        
-        fontLoginMessage = new FontCi();
-        fontLoginMessage.size = 14;
+		fontLoginMessage = new FontCi();
+		fontLoginMessage.size = 14;
 
-        title = "Login";
+		title = "Login";
 
-		AddWidget(login);
-		AddWidget(loginUsername);
-		AddWidget(loginPassword);
-		AddWidget(loginRememberMe);
-		AddWidget(createAccount);
-		AddWidget(createAccountUsername);
-		AddWidget(createAccountPassword);
-		AddWidget(createAccountRememberMe);
-		AddWidget(back);
+		loginResult = new LoginResultRef();
+	}
 
-        loginUsername.GetFocus();
+	ButtonWidget wbtn_back;
+	ButtonWidget wbtn_login;
+	ButtonWidget wbtn_createAccount;
+	TextBoxWidget wtbx_username;
+	TextBoxWidget wtbx_password;
+	CheckBoxWidget wcbx_rememberPassword;
+	TextWidget wtxt_title;
+	TextWidget wtxt_statusMessage;
+	TextWidget wtxt_username;
+	TextWidget wtxt_password;
 
-        loginResult = new LoginResultRef();
-    }
+	FontCi fontLoginMessage;
 
-    MenuWidget login;
-    MenuWidget loginUsername;
-    MenuWidget loginPassword;
-    MenuWidget loginRememberMe;
+	bool triedSavedLogin;
+	string title;
 
-    MenuWidget createAccount;
-    MenuWidget createAccountUsername;
-    MenuWidget createAccountPassword;
-    MenuWidget createAccountRememberMe;
+	public override void LoadTranslations()
+	{
+		wtxt_title.SetText(menu.lang.Get("MainMenu_Login"));
+		wtxt_username.SetText(menu.lang.Get("MainMenu_LoginUsername"));
+		wtxt_password.SetText(menu.lang.Get("MainMenu_LoginPassword"));
+		wcbx_rememberPassword.SetDescription(menu.lang.Get("MainMenu_LoginRemember"));
+		wbtn_login.SetText(menu.lang.Get("MainMenu_Login"));
+		wbtn_back.SetText(menu.lang.Get("MainMenu_ButtonBack"));
+		//wbtn_createAccount.SetText(menu.lang.Get("MainMenu_ButtonCreate")); // TODO: Add translation
+	}
 
-    MenuWidget back;
-    FontCi fontLoginMessage;
+	public override void Render(float dt)
+	{
+		if (!triedSavedLogin)
+		{
+			Preferences preferences = menu.p.GetPreferences();
+			wtbx_username.SetContent(menu.p, preferences.GetString("Username", ""));
+			string token = preferences.GetString("Password", "");
 
-    bool triedSavedLogin;
-    string title;
+			loginResultData = new LoginData();
+			if (serverHash != null && token != "")
+			{
+				menu.Login(wtbx_username.GetContent(), wtbx_password.GetContent(), serverHash, token, loginResult, loginResultData);
+			}
 
-    public override void LoadTranslations()
-    {
-        login.text = menu.lang.Get("MainMenu_Login");
-        loginUsername.description = menu.lang.Get("MainMenu_LoginUsername");
-        loginPassword.description = menu.lang.Get("MainMenu_LoginPassword");
-        loginRememberMe.text = menu.lang.Get("MainMenu_ChoiceYes");
-        loginRememberMe.description = menu.lang.Get("MainMenu_LoginRemember");
-        back.text = menu.lang.Get("MainMenu_ButtonBack");
-        title = menu.lang.Get("MainMenu_Login");
-    }
+			triedSavedLogin = true;
+		}
+		if (loginResultData != null
+			&& loginResultData.ServerCorrect
+			&& loginResultData.PasswordCorrect)
+		{
+			if (wcbx_rememberPassword.IsChecked())
+			{
+				Preferences preferences = menu.p.GetPreferences();
+				preferences.SetString("Username", wtbx_username.GetContent());
+				if (loginResultData.Token != null && loginResultData.Token != "")
+				{
+					preferences.SetString("Password", loginResultData.Token);
+				}
+				menu.p.SetPreferences(preferences);
+			}
+			menu.ConnectToGame(loginResultData, wtbx_username.GetContent());
+		}
 
-    public override void Render(float dt)
-    {
-        if (!triedSavedLogin)
-        {
-            Preferences preferences = menu.p.GetPreferences();
-            loginUsername.text = preferences.GetString("Username", "");
-            loginPassword.text = "";
-            string token = preferences.GetString("Password", "");
+		GamePlatform p = menu.p;
+		float scale = menu.GetScale();
 
-            loginResultData = new LoginData();
-            if (serverHash != null && token != "")
-            {
-                menu.Login(loginUsername.text, loginPassword.text, serverHash, token, loginResult, loginResultData);
-            }
+		menu.DrawBackground();
 
-            triedSavedLogin = true;
-        }
-        if (loginResultData != null
-            && loginResultData.ServerCorrect
-            && loginResultData.PasswordCorrect)
-        {
-            if (loginRememberMe.text == menu.lang.Get("MainMenu_ChoiceYes"))
-            {
-                Preferences preferences = menu.p.GetPreferences();
-                preferences.SetString("Username", loginUsername.text);
-                if (loginResultData.Token != null && loginResultData.Token != "")
-                {
-                    preferences.SetString("Password", loginResultData.Token);
-                }
-                menu.p.SetPreferences(preferences);
-            }
-            menu.ConnectToGame(loginResultData, loginUsername.text);
-        }
+		float leftx = p.GetCanvasWidth() / 2 - 400 * scale;
+		float y = p.GetCanvasHeight() / 2 - 250 * scale;
 
-        GamePlatform p = menu.p;
-        float scale = menu.GetScale();
+		string loginResultText = null;
+		if (loginResult.value == LoginResult.Failed)
+		{
+			loginResultText = menu.lang.Get("MainMenu_LoginInvalid");
+		}
+		if (loginResult.value == LoginResult.Connecting)
+		{
+			loginResultText = menu.lang.Get("MainMenu_LoginConnecting");
+		}
+		if (loginResultText != null)
+		{
+			wtxt_statusMessage.SetText(loginResultText);
+		}
 
-        menu.DrawBackground();
+		float rightx = p.GetCanvasWidth() / 2 + 150 * scale;
 
-        float leftx = p.GetCanvasWidth() / 2 - 400 * scale;
-        float y = p.GetCanvasHeight() / 2 - 250 * scale;
+		wtxt_title.x = leftx;
+		wtxt_title.y = y - 50 * scale;
+		wtxt_statusMessage.x = leftx;
+		wtxt_statusMessage.y = y;
 
-        string loginResultText = null;
-        if (loginResult.value == LoginResult.Failed)
-        {
-            loginResultText = menu.lang.Get("MainMenu_LoginInvalid");
-        }
-        if (loginResult.value == LoginResult.Connecting)
-        {
-            loginResultText = menu.lang.Get("MainMenu_LoginConnecting");
-        }
-        if (loginResultText != null)
-        {
-            menu.DrawText(loginResultText, fontLoginMessage, leftx, y - 50 * scale, TextAlign.Left, TextBaseline.Top);
-        }
+		const int textboxHeight = 64;
 
-        menu.DrawText(title, menu.fontMenuHeading, leftx, y + 50 * scale, TextAlign.Left, TextBaseline.Top);
+		float originx = leftx;
+		float originy = y + 100;
+		wtxt_username.x = originx;
+		wtxt_username.y = (originy + textboxHeight / 2) * scale;
+		wtbx_username.x = originx;
+		wtbx_username.y = originy * scale;
+		wtbx_username.sizex = 256 * scale;
+		wtbx_username.sizey = textboxHeight * scale;
 
-        loginUsername.x = leftx;
-        loginUsername.y = y + 100 * scale;
-        loginUsername.sizex = 256 * scale;
-        loginUsername.sizey = 64 * scale;
+		originy = y + 200;
+		wtxt_password.x = originx;
+		wtxt_password.y = (originy + textboxHeight / 2) * scale;
+		wtbx_password.x = originx;
+		wtbx_password.y = originy * scale;
+		wtbx_password.sizex = 256 * scale;
+		wtbx_password.sizey = textboxHeight * scale;
 
-        loginPassword.x = leftx;
-        loginPassword.y = y + 200 * scale;
-        loginPassword.sizex = 256 * scale;
-        loginPassword.sizey = 64 * scale;
+		wcbx_rememberPassword.x = leftx;
+		wcbx_rememberPassword.y = y + 300 * scale;
+		wcbx_rememberPassword.sizex = 256 * scale;
+		wcbx_rememberPassword.sizey = 32 * scale;
 
-        loginRememberMe.x = leftx;
-        loginRememberMe.y = y + 300 * scale;
-        loginRememberMe.sizex = 256 * scale;
-        loginRememberMe.sizey = 64 * scale;
+		wbtn_back.x = 40 * scale;
+		wbtn_back.y = p.GetCanvasHeight() - 104 * scale;
+		wbtn_back.sizex = 256 * scale;
+		wbtn_back.sizey = 64 * scale;
 
-        login.x = leftx;
-        login.y = y + 400 * scale;
-        login.sizex = 256 * scale;
-        login.sizey = 64 * scale;
+		wbtn_login.x = leftx;
+		wbtn_login.y = y + 400 * scale;
+		wbtn_login.sizex = 256 * scale;
+		wbtn_login.sizey = 64 * scale;
 
-        float rightx = p.GetCanvasWidth() / 2 + 150 * scale;
+		wbtn_createAccount.x = rightx;
+		wbtn_createAccount.y = y + 400 * scale;
+		wbtn_createAccount.sizex = 256 * scale;
+		wbtn_createAccount.sizey = 64 * scale;
 
-        // menu.DrawText("Create account", 14 * scale, rightx, y + 50 * scale, TextAlign.Left, TextBaseline.Top);
+		DrawWidgets();
+	}
 
-        createAccountUsername.x = rightx;
-        createAccountUsername.y = y + 100 * scale;
-        createAccountUsername.sizex = 256 * scale;
-        createAccountUsername.sizey = 64 * scale;
+	public override void OnBackPressed()
+	{
+		menu.StartMultiplayer();
+	}
 
-        createAccountPassword.x = rightx;
-        createAccountPassword.y = y + 200 * scale;
-        createAccountPassword.sizex = 256 * scale;
-        createAccountPassword.sizey = 64 * scale;
+	LoginResultRef loginResult;
+	LoginData loginResultData;
 
-        createAccountRememberMe.x = rightx;
-        createAccountRememberMe.y = y + 300 * scale;
-        createAccountRememberMe.sizex = 256 * scale;
-        createAccountRememberMe.sizey = 64 * scale;
+	public override void OnButtonA(AbstractMenuWidget w)
+	{
+		if (w == wbtn_login)
+		{
+			loginResultData = new LoginData();
+			if (serverHash != null)
+			{
+				// Connect to server hash, through main game menu. Do login.
+				menu.Login(wtbx_username.GetContent(), wtbx_password.GetContent(), serverHash, "", loginResult, loginResultData);
+			}
+			else
+			{
+				// Connect to IP. Don't login
 
-        createAccount.x = rightx;
-        createAccount.y = y + 400 * scale;
-        createAccount.sizex = 256 * scale;
-        createAccount.sizey = 64 * scale;
+				// Save username
+				if (wcbx_rememberPassword.IsChecked())
+				{
+					Preferences preferences = menu.p.GetPreferences();
+					preferences.SetString("Username", wtbx_username.GetContent());
+					menu.p.SetPreferences(preferences);
+				}
 
-        createAccountUsername.visible = false;
-        createAccountPassword.visible = false;
-        createAccountRememberMe.visible = false;
-        createAccount.visible = false;
-
-        back.x = 40 * scale;
-        back.y = p.GetCanvasHeight() - 104 * scale;
-        back.sizex = 256 * scale;
-        back.sizey = 64 * scale;
-
-        DrawWidgets();
-    }
-
-    public override void OnBackPressed()
-    {
-        menu.StartMultiplayer();
-    }
-
-    LoginResultRef loginResult;
-    LoginData loginResultData;
-
-    public override void OnButton(MenuWidget w)
-    {
-        if (w == login)
-        {
-            loginResultData = new LoginData();
-            if (serverHash != null)
-            {
-                // Connect to server hash, through main game menu. Do login.
-                menu.Login(loginUsername.text, loginPassword.text, serverHash, "", loginResult, loginResultData);
-            }
-            else
-            {
-                // Connect to IP. Don't login
-
-                // Save username
-                if (loginRememberMe.text == menu.lang.Get("MainMenu_ChoiceYes"))
-                {
-                    Preferences preferences = menu.p.GetPreferences();
-                    preferences.SetString("Username", loginUsername.text);
-                    menu.p.SetPreferences(preferences);
-                }
-
-                ConnectData connectdata = new ConnectData();
-                connectdata.Ip = serverIp;
-                connectdata.Port = serverPort;
-                connectdata.Username = loginUsername.text;
-                menu.StartGame(false, null, connectdata);
-            }
-        }
-        if (w == createAccount)
-        {
-            menu.CreateAccount(createAccountUsername.text, createAccountPassword.text, loginResult);
-        }
-        if (w == loginRememberMe || w == createAccountRememberMe)
-        {
-            if (w.text == menu.lang.Get("MainMenu_ChoiceYes"))
-            {
-                w.text = menu.lang.Get("MainMenu_ChoiceNo");
-            }
-            else
-            {
-                w.text = menu.lang.Get("MainMenu_ChoiceYes");
-            }
-        }
-        if (w == back)
-        {
-            OnBackPressed();
-        }
-    }
-    internal string serverHash;
-    internal string serverIp;
-    internal int serverPort;
+				ConnectData connectdata = new ConnectData();
+				connectdata.Ip = serverIp;
+				connectdata.Port = serverPort;
+				connectdata.Username = wtbx_username.GetContent();
+				menu.StartGame(false, null, connectdata);
+			}
+		}
+		if (w == wbtn_createAccount)
+		{
+			menu.CreateAccount(wtbx_username.GetContent(), wtbx_password.GetContent(), loginResult);
+		}
+		if (w == wbtn_back)
+		{
+			OnBackPressed();
+		}
+	}
+	internal string serverHash;
+	internal string serverIp;
+	internal int serverPort;
 }
