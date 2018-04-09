020ea5faf7a54cc4ce4a1809e709053cf3902815
croxxx <i.am.croxxx@aol.de>
2018-04-09 18:22:23 +0200

    ScreenModifyWorld: Formatting

--- ManicDiggerLib/Client/UI/Screens/ModifyWorld.ci.cs
+++ ManicDiggerLib/Client/UI/Screens/ModifyWorld.ci.cs
@@ -1,43 +1,43 @@
 ﻿public class ScreenModifyWorld : Screen
 {
-    public ScreenModifyWorld()
-    {
-        back = new MenuWidget();
-        back.text = "Back";
-        back.type = WidgetType.Button;
+	public ScreenModifyWorld()
+	{
+		back = new MenuWidget();
+		back.text = "Back";
+		back.type = WidgetType.Button;
 
 		AddWidget(back);
-    }
+	}
 
-    MenuWidget back;
+	MenuWidget back;
 
-    public override void Render(float dt)
-    {
-        GamePlatform p = menu.p;
+	public override void Render(float dt)
+	{
+		GamePlatform p = menu.p;
 
-        float scale = menu.GetScale();
+		float scale = menu.GetScale();
 
-        menu.DrawBackground();
-        menu.DrawText("Modify World", menu.fontMenuHeading, menu.p.GetCanvasWidth() / 2, 0, TextAlign.Center, TextBaseline.Top);
+		menu.DrawBackground();
+		menu.DrawText("Modify World", menu.fontMenuHeading, menu.p.GetCanvasWidth() / 2, 0, TextAlign.Center, TextBaseline.Top);
 
-        back.x = 40 * scale;
-        back.y = p.GetCanvasHeight() - 104 * scale;
-        back.sizex = 256 * scale;
-        back.sizey = 64 * scale;
+		back.x = 40 * scale;
+		back.y = p.GetCanvasHeight() - 104 * scale;
+		back.sizex = 256 * scale;
+		back.sizey = 64 * scale;
 
-        DrawWidgets();
-    }
+		DrawWidgets();
+	}
 
-    public override void OnBackPressed()
-    {
-        menu.StartSingleplayer();
-    }
+	public override void OnBackPressed()
+	{
+		menu.StartSingleplayer();
+	}
 
-    public override void OnButton(MenuWidget w)
-    {
-        if (w == back)
-        {
-            OnBackPressed();
-        }
-    }
+	public override void OnButton(MenuWidget w)
+	{
+		if (w == back)
+		{
+			OnBackPressed();
+		}
+	}
 }
