b0b3812f775606852de148c3cdafc0f6ebf388e3
croxxx <i.am.croxxx@aol.de>
2018-04-15 18:38:08 +0200

    Add unit test for AnimatedBackgroundWidget class

---
+++ ManicDigger.Tests/Client/UI/Widgets/AnimatedBackgroundWidgetTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+	[TestFixture()]
+	public class AnimatedBackgroundWidgetTests
+	{
+		private AnimatedBackgroundWidget w;
+
+		[SetUp]
+		public void AnimatedBackgroundWidgetTest()
+		{
+			w = new AnimatedBackgroundWidget();
+		}
+
+		[Test()]
+		public void InitTest()
+		{
+			w.Init(null, 10, 20);
+			Assert.That(w.GetSizeX(), Is.EqualTo(10));
+			Assert.That(w.GetSizeY(), Is.EqualTo(20));
+		}
+	}
+}
