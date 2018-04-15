537a34e97fee63f1bc8824bc4f40708d26d094a0
croxxx <i.am.croxxx@aol.de>
2018-04-15 18:37:31 +0200

    Add unit tests for AbstractMenuWidget class

---
+++ ManicDigger.Tests/Client/UI/AbstractMenuWidgetTests.cs
@@ -0,0 +1,308 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+	[TestFixture(typeof(AnimatedBackgroundWidget))]
+	[TestFixture(typeof(ButtonWidget))]
+	[TestFixture(typeof(CheckBoxWidget))]
+	[TestFixture(typeof(ImageWidget))]
+	[TestFixture(typeof(ListWidget))]
+	[TestFixture(typeof(ProgressBarWidget))]
+	[TestFixture(typeof(ServerButtonWidget))]
+	[TestFixture(typeof(TextBoxWidget))]
+	[TestFixture(typeof(TextWidget))]
+	public class AbstractMenuWidgetTests<TWidget> where TWidget : AbstractMenuWidget, new()
+	{
+		private AbstractMenuWidget w;
+
+		[SetUp]
+		public void SetUp()
+		{
+			this.w = new TWidget();
+		}
+
+		[Test()]
+		public void ConstructorTest()
+		{
+			Assert.That(w, Is.TypeOf(typeof(TWidget)));
+			Assert.That(w, Is.InstanceOf(typeof(TWidget)));
+			Assert.That(w, Is.InstanceOf(typeof(AbstractMenuWidget)));
+			Assert.That(w.GetX(), Is.EqualTo(0));
+			Assert.That(w.GetY(), Is.EqualTo(0));
+			Assert.That(w.GetSizeX(), Is.EqualTo(0));
+			Assert.That(w.GetSizeY(), Is.EqualTo(0));
+			Assert.That(w.GetColor(), Is.EqualTo(-1));
+			Assert.That(w.GetEventKeyPressed(), Is.EqualTo(false));
+			Assert.That(w.GetEventName(), Is.EqualTo(null));
+			Assert.That(w.GetEventResponse(), Is.EqualTo(null));
+			Assert.That(w.GetFocused(), Is.EqualTo(false));
+			Assert.That(w.GetVisible(), Is.EqualTo(true));
+		}
+
+		[Test()]
+		public void OnKeyPressTest()
+		{
+			KeyPressEventArgs args = new KeyPressEventArgs();
+			args.SetHandled(false);
+			args.SetKeyChar(1);
+
+			// false after initialization
+			Assert.That(w.GetEventKeyPressed(), Is.EqualTo(false));
+			// false when no key is set
+			w.OnKeyPress(null, args);
+			Assert.That(w.GetEventKeyPressed(), Is.EqualTo(false));
+			// false when wrong key is set
+			w.SetEventKeyChar(2);
+			Assert.That(w.GetEventKeyPressed(), Is.EqualTo(false));
+			w.OnKeyPress(null, args);
+			Assert.That(w.GetEventKeyPressed(), Is.EqualTo(false));
+			// true when right key is set
+			w.SetEventKeyChar(1);
+			Assert.That(w.GetEventKeyPressed(), Is.EqualTo(false));
+			w.OnKeyPress(null, args);
+			Assert.That(w.GetEventKeyPressed(), Is.EqualTo(true));
+			// true when event is repeated
+			w.OnKeyPress(null, args);
+			Assert.That(w.GetEventKeyPressed(), Is.EqualTo(true));
+			// false when wrong key is set again
+			w.SetEventKeyChar(2);
+			Assert.That(w.GetEventKeyPressed(), Is.EqualTo(false));
+			w.OnKeyPress(null, args);
+			Assert.That(w.GetEventKeyPressed(), Is.EqualTo(false));
+		}
+
+		[Test()]
+		public void OnKeyDownTest()
+		{
+			// no common logic yet
+			Assert.Pass();
+		}
+
+		[Test()]
+		public void OnMouseDownTest()
+		{
+			// no common logic yet
+			Assert.Pass();
+		}
+
+		[Test()]
+		public void OnMouseUpTest()
+		{
+			// no common logic yet
+			Assert.Pass();
+		}
+
+		[Test()]
+		public void OnMouseMoveTest()
+		{
+			// no common logic yet
+			Assert.Pass();
+		}
+
+		[Test()]
+		public void OnMouseWheelTest()
+		{
+			// no common logic yet
+			Assert.Pass();
+		}
+
+		[Test()]
+		public void IsCursorInsideTest()
+		{
+			MouseEventArgs args = new MouseEventArgs();
+			MouseEventArgs args2 = new MouseEventArgs();
+			Assert.That(w.IsCursorInside(args), Is.EqualTo(true));
+			args.SetX(10);
+			args.SetY(20);
+			args2.SetX(-1);
+			args2.SetY(-1);
+			Assert.That(w.IsCursorInside(args), Is.EqualTo(false));
+			Assert.That(w.IsCursorInside(args2), Is.EqualTo(false));
+			w.SetSizeX(5);
+			w.SetSizeY(5);
+			Assert.That(w.IsCursorInside(args), Is.EqualTo(false));
+			Assert.That(w.IsCursorInside(args2), Is.EqualTo(false));
+			w.SetSizeX(10);
+			w.SetSizeY(5);
+			Assert.That(w.IsCursorInside(args), Is.EqualTo(false));
+			Assert.That(w.IsCursorInside(args2), Is.EqualTo(false));
+			w.SetSizeX(5);
+			w.SetSizeY(20);
+			Assert.That(w.IsCursorInside(args), Is.EqualTo(false));
+			Assert.That(w.IsCursorInside(args2), Is.EqualTo(false));
+			w.SetSizeX(10);
+			w.SetSizeY(20);
+			Assert.That(w.IsCursorInside(args), Is.EqualTo(true));
+			Assert.That(w.IsCursorInside(args2), Is.EqualTo(false));
+		}
+
+		[Test()]
+		public void FocusedTest()
+		{
+			Assert.That(w.GetFocused(), Is.EqualTo(false));
+			w.SetFocused(true);
+			if (w.GetFocusable())
+			{
+				Assert.That(w.GetFocused(), Is.EqualTo(true));
+			}
+			else
+			{
+				Assert.That(w.GetFocused(), Is.EqualTo(false));
+			}
+			w.SetFocused(false);
+			Assert.That(w.GetFocused(), Is.EqualTo(false));
+		}
+
+		[Test()]
+		public void VisibleTest()
+		{
+			Assert.That(w.GetVisible(), Is.EqualTo(true));
+			w.SetVisible(false);
+			Assert.That(w.GetVisible(), Is.EqualTo(false));
+			w.SetVisible(true);
+			Assert.That(w.GetVisible(), Is.EqualTo(true));
+		}
+
+		[Test()]
+		public void ClickableTest()
+		{
+			if (w.GetClickable())
+			{
+				w.SetClickable(true);
+				Assert.That(w.GetClickable(), Is.EqualTo(true));
+			}
+			else
+			{
+				w.SetClickable(true);
+				Assert.That(w.GetClickable(), Is.EqualTo(true));
+			}
+			w.SetClickable(false);
+			Assert.That(w.GetClickable(), Is.EqualTo(false));
+		}
+
+		[Test()]
+		public void FocusableTest()
+		{
+			w.SetFocused(true);
+			if (w.GetFocusable())
+			{
+				Assert.That(w.GetFocused(), Is.EqualTo(true));
+			}
+			else
+			{
+				Assert.That(w.GetFocused(), Is.EqualTo(false));
+			}
+			w.SetFocusable(false);
+			Assert.That(w.GetFocusable(), Is.EqualTo(false));
+			Assert.That(w.GetFocused(), Is.EqualTo(false));
+		}
+
+		[Test()]
+		public void HasBeenClickedTest()
+		{
+			MouseEventArgs args = new MouseEventArgs();
+			MouseEventArgs args2 = new MouseEventArgs();
+			Assert.That(w.HasBeenClicked(args), Is.EqualTo(w.GetClickable()));
+			args.SetX(10);
+			args.SetY(20);
+			args2.SetX(-1);
+			args2.SetY(-1);
+			Assert.That(w.HasBeenClicked(args), Is.EqualTo(false));
+			Assert.That(w.HasBeenClicked(args2), Is.EqualTo(false));
+			w.SetSizeX(5);
+			w.SetSizeY(5);
+			Assert.That(w.HasBeenClicked(args), Is.EqualTo(false));
+			Assert.That(w.HasBeenClicked(args2), Is.EqualTo(false));
+			w.SetSizeX(10);
+			w.SetSizeY(5);
+			Assert.That(w.HasBeenClicked(args), Is.EqualTo(false));
+			Assert.That(w.HasBeenClicked(args2), Is.EqualTo(false));
+			w.SetSizeX(5);
+			w.SetSizeY(20);
+			Assert.That(w.HasBeenClicked(args), Is.EqualTo(false));
+			Assert.That(w.HasBeenClicked(args2), Is.EqualTo(false));
+			w.SetSizeX(10);
+			w.SetSizeY(20);
+			Assert.That(w.HasBeenClicked(args), Is.EqualTo(w.GetClickable()));
+			Assert.That(w.HasBeenClicked(args2), Is.EqualTo(false));
+		}
+
+		[Test()]
+		public void SetXTest()
+		{
+			int val = 5;
+			Assert.That(w.GetX(), Is.EqualTo(0));
+			w.SetX(val);
+			Assert.That(w.GetX(), Is.EqualTo(val));
+			w.SetX(-val);
+			Assert.That(w.GetX(), Is.EqualTo(-val));
+		}
+
+		[Test()]
+		public void SetYTest()
+		{
+			int val = 5;
+			Assert.That(w.GetY(), Is.EqualTo(0));
+			w.SetY(val);
+			Assert.That(w.GetY(), Is.EqualTo(val));
+			w.SetY(-val);
+			Assert.That(w.GetY(), Is.EqualTo(-val));
+		}
+
+		[Test()]
+		public void SetSizeXTest()
+		{
+			int val = 5;
+			Assert.That(w.GetSizeX(), Is.EqualTo(0));
+			w.SetSizeX(val);
+			Assert.That(w.GetSizeX(), Is.EqualTo(val));
+			w.SetSizeX(-val);
+			Assert.That(w.GetSizeX(), Is.EqualTo(-val));
+		}
+
+		[Test()]
+		public void SetSizeYTest()
+		{
+			int val = 5;
+			Assert.That(w.GetSizeY(), Is.EqualTo(0));
+			w.SetSizeY(val);
+			Assert.That(w.GetSizeY(), Is.EqualTo(val));
+			w.SetSizeY(-val);
+			Assert.That(w.GetSizeY(), Is.EqualTo(-val));
+		}
+
+		[Test()]
+		public void SetColorTest()
+		{
+			int col = ColorCi.FromArgb(0, 0, 0, 0);
+			Assert.That(w.GetColor(), Is.EqualTo(ColorCi.FromArgb(255, 255, 255, 255)));
+			w.SetColor(col);
+			Assert.That(w.GetSizeY(), Is.EqualTo(col));
+		}
+
+		[Test()]
+		public void SetEventKeyCharTest()
+		{
+			string val = "test";
+			Assert.That(w.GetEventKeyPressed(), Is.EqualTo(false));
+			w.SetEventName(val);
+			Assert.That(w.GetEventKeyPressed(), Is.EqualTo(false));
+		}
+
+		[Test()]
+		public void SetEventNameTest()
+		{
+			string val = "test";
+			Assert.That(w.GetEventName(), Is.EqualTo(null));
+			w.SetEventName(val);
+			Assert.That(w.GetEventName(), Is.EqualTo(val));
+			w.SetEventName(null);
+			Assert.That(w.GetEventName(), Is.EqualTo(null));
+		}
+		[Test()]
+		public void GetEventResponseTest()
+		{
+			Assert.That(w.GetEventResponse(), Is.EqualTo(null));
+		}
+	}
+}
